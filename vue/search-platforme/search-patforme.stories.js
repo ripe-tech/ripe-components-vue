9a8a85c6f9d2a91f6aff7df1e9bcdb4e76efeeeb
joamag <joamag@hive.pt>
2019-05-08 10:51:48 +0100

    New search platforme component structure

--- vue/search-platforme/search-patforme.stories.js
+++ vue/search-platforme/search-patforme.stories.js
@@ -6,7 +6,7 @@ storiesOf("Input", module)
     .add("Search", () => ({
         props: {
             iconVisible: {
-                default: boolean("Icon Visible", true)
+                default: boolean("Icon Visible", false)
             }
         },
         template: "<search-platforme v-bind:icon-visible='iconVisible'></search-platforme>"
--- vue/search-platforme/search-platforme.vue
+++ vue/search-platforme/search-platforme.vue
@@ -1,15 +1,16 @@
 <template>
     <div
         class="search-platforme"
-        v-bind:class="[focused ? 'focus' : 'unfocus', grow ? 'grow' : '']"
+        v-bind:class="[focused ? 'focus' : 'unfocus', grow ? 'grow' : '', iconVisible ? '' : 'icon-invisible']"
     >
-        <slot name="icon" v-show="iconVisible">
+        <slot name="icon">
             <svg
                 focusable="false"
                 height="24px"
                 viewBox="0 0 24 24"
                 width="24px"
                 xmlns="http://www.w3.org/2000/svg"
+                v-show="iconVisible"
             >
                 <path
                     d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
@@ -75,8 +76,14 @@
     line-height: 32px;
     margin-left: 24px;
     padding-left: 33px;
+    padding-right: 12px;
     transition: width 0.2s ease, border-color 0.2s ease, background-color 0.2s ease;
     width: 260px;
+    box-sizing: border-box;
+}
+
+.search-platforme.icon-invisible > input[type="text"] {
+    padding-left: 12px;
 }
 
 .search-platforme > input[type="text"]:hover {
