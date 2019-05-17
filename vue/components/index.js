2ee2e9938de4188583b50db948e37ad9d6d30f3b
joamag <joamag@hive.pt>
2019-05-17 15:02:14 +0100

    Major re-structure of the vue components

---
+++ vue/components/index.js
@@ -0,0 +1,48 @@
+import { ButtonPlatforme } from "./button-platforme/button-platforme.vue";
+import { ContainerPlatforme } from "./container-platforme/container-platforme.vue";
+import { DropdownPlatforme } from "./dropdown-platforme/dropdown-platforme.vue";
+import { FilterPlatforme } from "./filter-platforme/filter-platforme.vue";
+import { FooterPlatforme } from "./footer-platforme/footer-platforme.vue";
+import { HeaderPlatforme } from "./header-platforme/header-platforme.vue";
+import { LightboxPlatforme } from "./lightbox-platforme/lightbox-platforme.vue";
+import { LoaderPlatforme } from "./loader-platforme/loader-platforme.vue";
+import { OverlayPlatforme } from "./overlay-platforme/overlay-platforme.vue";
+import { ProgressBarPlatforme } from "./progress-bar-platforme/progress-bar-platforme.vue";
+import { SearchPlatforme } from "./search-platforme/search-platforme.vue";
+import { SidePlatforme } from "./side-platforme/side-platforme.vue";
+import { TablePlatforme } from "./table-platforme/table-platforme.vue";
+
+const install = Vue => {
+    Vue.component("button-platforme", ButtonPlatforme);
+    Vue.component("container-platforme", ContainerPlatforme);
+    Vue.component("dropdown-platforme", DropdownPlatforme);
+    Vue.component("filter-platforme", FilterPlatforme);
+    Vue.component("footer-platforme", FooterPlatforme);
+    Vue.component("header-platforme", HeaderPlatforme);
+    Vue.component("loader-platforme", LoaderPlatforme);
+    Vue.component("lightbox-platforme", LightboxPlatforme);
+    Vue.component("overlay-platforme", OverlayPlatforme);
+    Vue.component("progress-bar-platforme", ProgressBarPlatforme);
+    Vue.component("search-platforme", SearchPlatforme);
+    Vue.component("side-platforme", SidePlatforme);
+    Vue.component("table-platforme", TablePlatforme);
+};
+
+export {
+    ButtonPlatforme,
+    ContainerPlatforme,
+    DropdownPlatforme,
+    FilterPlatforme,
+    FooterPlatforme,
+    HeaderPlatforme,
+    LoaderPlatforme,
+    LightboxPlatforme,
+    OverlayPlatforme,
+    ProgressBarPlatforme,
+    SearchPlatforme,
+    SidePlatforme,
+    TablePlatforme,
+    install
+};
+
+export default install;
---
+++ vue/mixins/ripe.js
@@ -0,0 +1,47 @@
+const ripeMixin = {
+    methods: {
+        toTokensM(tokens) {
+            const tokensM = {};
+
+            for (const token of tokens) {
+                let tokensC = tokensM;
+                const tokenL = token.split(".");
+                const head = tokenL.slice(0, tokenL.length - 1);
+                const tail = tokenL[tokenL.length - 1];
+
+                for (const tokenP of head) {
+                    let current = tokensC[tokenP] || {};
+                    const isDict = typeof current === "object";
+                    if (!isDict) current = { _: current };
+                    tokensC[tokenP] = current;
+                    tokensC = current;
+                }
+
+                const leaf = tokensC[tail] || null;
+                if (leaf && typeof leaf === "object") leaf._ = true;
+                else tokensC[tail] = true;
+            }
+
+            return tokensM;
+        },
+        hasPermission(token, tokensM = null) {
+            tokensM = tokensM || this.tokens;
+            if (!token) return true;
+
+            const tokenL = token.split(".");
+            for (const tokenP of tokenL) {
+                if (typeof tokensM !== "object") return false;
+                if (tokensM["*"]) return true;
+                if (tokensM[tokenP] === undefined) return false;
+                tokensM = tokensM[tokenP];
+            }
+
+            const isDict = typeof tokensM === "object";
+            const result = isDict ? tokensM["_"] || false : tokensM;
+
+            return Boolean(result);
+        }
+    }
+};
+
+export { ripeMixin };
