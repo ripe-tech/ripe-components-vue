b3e34dce63d35a7804373f5edbbdf47207389303
joamag <joamag@hive.pt>
2019-05-17 16:21:06 +0100

    Created new shared mixin

--- vue/mixins/index.js
+++ vue/mixins/index.js
@@ -1,1 +1,2 @@
+export * from "./part";
 export * from "./ripe";
---
+++ vue/mixins/part.js
@@ -0,0 +1,36 @@
+const partMixin = {
+    methods: {
+        /**
+         * Handles the error in the UI so that the user is
+         * properly notified about the problem.
+         * Typical handling involves router redirection.
+         *
+         * @param {Error} err The error object to be used in the
+         * handling of the error.
+         * @param {String} message The message to be used in for the
+         * error handling if not provided the default one extracted
+         * from the error is used.
+         */
+        handleError(err, message, code) {
+            code = code || err.code;
+            const query = { message: message || err.message };
+            if (code) query.code = code;
+            this.$router.push({
+                name: "error",
+                query: query
+            });
+        },
+        hasPermission(token) {
+            return this.$root.hasPermission(token);
+        },
+        ensurePermission(token) {
+            if (!this.hasPermission(token)) {
+                this.handleError(null, "Not enough permissions");
+                return false;
+            }
+            return true;
+        }
+    }
+};
+
+export { partMixin };
