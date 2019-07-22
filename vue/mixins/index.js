0b23d5612ff2927faaea4adadde73f703d89a027
joamag <joamag@hive.pt>
2019-07-22 15:36:37 +0100

    New utils mixin

--- vue/mixins/index.js
+++ vue/mixins/index.js
@@ -1,3 +1,4 @@
 export * from "./oauth";
 export * from "./part";
 export * from "./ripe";
+export * from "./utils";
---
+++ vue/mixins/utils.js
@@ -0,0 +1,39 @@
+const utilsMixin = {
+    methods: {
+        dateString(timestamp, separator = "/") {
+            const date = new Date(timestamp * 1000);
+            const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+            let month = date.getMonth() + 1;
+            month = month < 10 ? `0${month}` : month;
+            return day + separator + month + separator + date.getFullYear();
+        },
+        dateStringUTC(timestamp, separator = "/") {
+            const date = new Date(timestamp * 1000);
+            const day = date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate();
+            let month = date.getUTCMonth() + 1;
+            month = month < 10 ? `0${month}` : month;
+            return day + separator + month + separator + date.getUTCFullYear();
+        },
+        timeString(timestamp, separator = ":") {
+            const date = new Date(timestamp * 1000);
+            const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+            const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+            const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+            return hours + separator + minutes + separator + seconds;
+        },
+        timeStringUTC(timestamp, separator = ":") {
+            const date = new Date(timestamp * 1000);
+            const hours = date.getUTCHours() < 10 ? `0${date.getUTCHours()}` : date.getUTCHours();
+            const minutes =
+                date.getUTCMinutes() < 10 ? `0${date.getUTCMinutes()}` : date.getUTCMinutes();
+            const seconds =
+                date.getUTCSeconds() < 10 ? `0${date.getUTCSeconds()}` : date.getUTCSeconds();
+            return hours + separator + minutes + separator + seconds;
+        },
+        normalize(value) {
+            return value.split("_").join(" ");
+        }
+    }
+};
+
+export { utilsMixin };
