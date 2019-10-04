a0264943533d29f9705d1831ec8ff0096b73afce
3rdvision <kazgru@gmail.com>
2019-10-04 17:01:03 +0100

    Added generic field

---
+++ vue/components/ui/generic-field-platforme/generic-field-platforme.stories.js
@@ -0,0 +1,26 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, boolean, color, number, text, select } from "@storybook/addon-knobs";
+
+storiesOf("Input", module)
+    .addDecorator(withKnobs)
+    .add("Generic", () => ({
+        props: {
+            type: {
+                default: select("Type", ["Text","Number","Email","Password","Tel","Time","Url","Date","Range","Hidden"],"text")
+            },
+            placeholder: {
+                default: text("Placeholder Text", "Placeholder here")
+            },
+            required: {
+                default: boolean("Is Required", false)
+            },
+            autofocus: {
+                default: boolean("Autofocus", false)
+            }
+        },
+        template: `
+            <div>
+                <generic-field-platforme v-bind:placeholder='placeholder' v-bind:required='required' v-bind:autofocus='autofocus' v-bind:type='type' ></generic-field-platforme>
+            </div>
+        `
+    }));
---
+++ vue/components/ui/generic-field-platforme/generic-field-platforme.vue
@@ -0,0 +1,111 @@
+<template>
+    <div
+        class="generic-field-platforme"
+    >
+        <global-events v-on:keydown.esc="blur()" />
+        <input
+            v-bind:type="type"
+            v-bind:value="value"
+            v-bind:placeholder="placeholder"
+            v-bind:required='required'
+            v-bind:autofocus='autofocus'
+            ref="input"
+            v-on:input="$emit('update:value', $event.target.value)"
+            v-on:focus="focused = true"
+            v-on:blur="focused = false"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "css/variables.scss";
+
+.generic-field-platforme {
+    display: inline-block;
+    height: 34px;
+    line-height: 34px;
+}
+
+.generic-field-platforme > input {
+    background-color: #f2f2f2;
+    border: 1px solid transparent;
+    box-sizing: border-box;
+    font-size: 13px;
+    height: 34px;
+    line-height: 34px;
+    margin-left: 24px;
+    padding-left: 12px;
+    padding-right: 12px;
+    transition: width 0.2s ease, border-color 0.2s ease, background-color 0.2s ease;
+    width: 304px;
+}
+
+.generic-field-platforme > input:hover {
+    border-color: #dddddd;
+}
+
+.generic-field-platforme > input:focus {
+    background-color: #ffffff;
+    border-color: #dddddd;
+    box-shadow: 0px 1px 8px 0px rgba(32, 33, 36, 0.14);
+}
+
+.generic-field-platforme.grow > input:focus {
+    width: 340px;
+}
+
+</style>
+
+<script>
+export const genericFieldPlatforme = {
+    props: {
+        value: {
+            type: String,
+            default: null
+        },
+        placeholder: {
+            type: String,
+            default: "Name"
+        },
+        autofocus: {
+            type: Boolean,
+            default: false
+        },
+        required: {
+            type: Boolean,
+            default: false
+        },
+        type: {
+            type: String,
+            default: "text"
+        }
+    },
+    data: function() {
+        return {
+            focused: false,
+            suggestionsVisible: false
+        };
+    },
+    watch: {
+        focused(value) {
+            if (value) {
+                this.suggestionsVisible = true;
+            } else {
+                setTimeout(() => {
+                    this.suggestionsVisible = false;
+                }, 100);
+            }
+        }
+    },
+    mounted: function() {
+        this.autofocus && this.$refs.input.focus();
+    },
+    methods: {
+        blur() {
+            this.$refs.input.blur();
+        }
+    }
+};
+
+export default genericFieldPlatforme;
+</script>
