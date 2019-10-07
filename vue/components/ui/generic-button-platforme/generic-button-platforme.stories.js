df8e90f8b0adfbf857266ce2fcec778cc066c33c
Mauro Benedito <v-mb@platforme.com>
2019-10-07 16:45:17 +0100

    button added to storybook. Arrow animation and scale animation configurable by props

---
+++ vue/components/ui/generic-button-platforme/generic-button-platforme.stories.js
@@ -0,0 +1,39 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, color, text, boolean } from "@storybook/addon-knobs";
+
+storiesOf("Generic Button", module)
+    .addDecorator(withKnobs)
+    .add("button", () => ({
+        props: {
+            scaleAnimation: {
+                default: boolean("Scale Animation", false)
+            },
+            arrowAnimation: {
+                default: boolean("Arrow Animation", true)
+            },
+            text: {
+                default: text("Button Text", "Click")
+            },
+            textColor: {
+                default: color("Text Color", "#ffffff")
+            },
+            borderColor: {
+                default: color("Border Color", "#40c4ff")
+            },
+            backgroundColor: {
+                default: color("Background Color", "#40c4ff")
+            }
+        },
+        template: `
+            <div>
+                <generic-button-platforme 
+                    v-bind:text="text"
+                    v-bind:backgroundColor="backgroundColor"
+                    v-bind:textColor="textColor"
+                    v-bind:borderColor="borderColor"
+                    v-bind:arrowAnimation="arrowAnimation"
+                    v-bind:scaleAnimation="scaleAnimation"
+                    />
+            </div>
+        `
+    }));
--- vue/components/ui/generic-button-platforme/generic-button-platforme.vue
+++ vue/components/ui/generic-button-platforme/generic-button-platforme.vue
@@ -1,15 +1,27 @@
 <template>
-    <button class="btn btn-content" v-bind:style="buttonStyleColor">
-        {{ buttonText }}
+    <button
+        class="button button-content"
+        v-bind:class="{ 
+            arrow : arrowAnimation,
+            scale : scaleAnimation
+         }"
+        
+        v-bind:style="{
+            'background-color': backgroundColor,
+            'color': textColor,
+            'border-color': borderColor
+        }"
+    >
+        {{ text }}
     </button>
 </template>
 
 <style scoped>
-.btn-content:hover:not(:disabled)::after {
-    right: 4px;
+.button-content.arrow:hover:not(:disabled)::after {
+    right: 0px;
 }
 
-.btn-content::after {
+.button-content.arrow::after {
     background-image: url(https://d3moonnr9fkxfg.cloudfront.net/royal-canin.sprite--brand3--xs.svg?v=8-7-8);
     background-position: 45.45% 85.71%;
     background-repeat: no-repeat;
@@ -20,17 +32,14 @@
     position: absolute;
     right: -32px;
     top: 50%;
-    -webkit-transform: translateY(-50%);
     transform: translateY(-50%);
-    -webkit-transform-origin: 50% 50%;
     transform-origin: 50% 50%;
-    -webkit-transition: 0.2s;
     transition: 0.2s;
     width: 32px;
 }
 
-.btn {
-    border: 2px solid transparent;
+.button {
+    border: 2px solid;
     border-radius: 999px;
     cursor: pointer;
     display: inline-block;
@@ -39,47 +48,30 @@
     line-height: inherit;
     outline: none;
     overflow: hidden;
-    padding: 6px 27px;
+    padding: 8px 30px 8px 30px;
     position: relative;
     text-align: center;
     text-decoration: none;
     text-overflow: ellipsis;
-    -webkit-transition: 0.2s;
     transition: 0.2s;
     vertical-align: middle;
     white-space: nowrap;
     width: auto;
 }
 
-body {
-    font-family: "RC TYPE", Roboto, Avenir, Helvetica, Arial, sans-serif;
+.button.scale:hover {
+    transform: scale(1.1);
 }
 
 button {
-    align-items: flex-start;
-    -webkit-appearance: button;
     appearance: button;
-    border-color: rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186);
     border-image: initial;
     border-style: solid;
-    border-width: 1px;
-    box-sizing: border-box;
-    display: inline-block;
-    font: 400 11px system-ui;
-    letter-spacing: normal;
-    margin: 0em;
+    margin: 0px 0px 0px 0px;
     padding: 1px 7px 2px;
-    text-align: center;
-    text-indent: 0px;
-    text-rendering: auto;
-    text-shadow: none;
-    text-transform: none;
-    word-spacing: normal;
-    -webkit-writing-mode: horizontal-tb !important;
-    writing-mode: horizontal-tb !important;
 }
 
-.btn-content {
+.button-content {
     font-weight: 500;
 }
 </style>
@@ -87,21 +79,8 @@ button {
 <script>
 export const GenericButtonPlatforme = {
     name: "generic-button-platforme",
-    data() {
-        return {
-            buttonStyleColor:
-                "background-color: " +
-                this.backgroundColor +
-                "; " +
-                "color: " +
-                this.textColor +
-                "; " +
-                "border-color: " +
-                this.borderColor
-        };
-    },
     props: {
-        buttonText: {
+        text: {
             type: String,
             default: "Click"
         },
@@ -116,6 +95,14 @@ export const GenericButtonPlatforme = {
         borderColor: {
             type: String,
             default: "#40c4ff"
+        },
+        scaleAnimation: {
+            type: Boolean,
+            default: "False"
+        },
+        arrowAnimation: {
+            type: Boolean,
+            default: "False"
         }
     }
 };
