b0663ba255d9f8c74107a38d892a266b21076cba
3rdvision <kazgru@gmail.com>
2019-10-08 10:53:33 +0100

    Data in knob and cleanup

--- vue/components/ui/selector-platforme/selector-platforme.stories.js
+++ vue/components/ui/selector-platforme/selector-platforme.stories.js
@@ -1,38 +1,33 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 
 storiesOf("Input", module)
     .addDecorator(withKnobs)
     .add("Selector", () => ({
-        props: {
-            visible: {
-                default: boolean("Visible", true)
-            },
-            visibleData: {
-                default: boolean("Visible Data", false)
-            }
-        },
         data() {
             return {
                 items: [
                     {
-                        id: "text_1",
-                        text: "Text 1"
+                        id: "o1",
+                        value: "opt1",
+                        text: "White"
                     },
                     {
-                        id: "text_2",
-                        text: "Text 2"
+                        id: "o2",
+                        value: "opt2",
+                        text: "Yellow"
                     },
                     {
-                        id: "text_3",
-                        text: "Text 3"
+                        id: "o3",
+                        value: "opt3",
+                        text: "Red"
                     }
                 ]
             };
         },
         template: `
             <div style="max-width: 200px">
-                <selector-platforme v-bind:items="items" v-bind:visible="visible" v-bind:visibleData="visibleData"></selector-platforme>
+                <selector-platforme v-bind:items="items"></selector-platforme>
             </div>
         `
     }));
