47d729b99d18aef78843f8cc04b034ab00d18f39
Daniel Blazeque <v-db@platforme.com>
2019-11-15 16:59:02 +0000

    table fixed with filter-platforme

--- vue/components/ui/filter-platforme/filter-platforme.stories.js
+++ vue/components/ui/filter-platforme/filter-platforme.stories.js
@@ -9,15 +9,14 @@ storiesOf("Input", module).add("Filter", () => ({
                 { id: "name", title: "Name" },
                 { id: "car", title: "Car" }
             ]
-        }
-    },
-    data: function() {
-        return {
-            filterItems: [
+        },
+        getItems: {
+            type: Array,
+            default: () => [
                 {
                     id: 1,
                     name: "John",
-                    car: "Mercedes-Benz"
+                    car: "Mercedes Benz"
                 },
                 {
                     id: 2,
@@ -27,31 +26,67 @@ storiesOf("Input", module).add("Filter", () => ({
                 {
                     id: 3,
                     name: "Anna",
-                    car: "Land-Rover"
+                    car: "Land Rover"
                 }
             ]
-        };
+
+        }
+    },
+    computed: {
+        matches() {
+          return this.search
+            ? this.editions.filter(edition => {
+              let match = false
+              for (let key in edition) {
+                if (edition[key].toLowerCase().includes(this.search.toLowerCase())) {
+                  return true
+                }
+              }
+            })
+            : this.editions
+        }
+      },        
+      methods: {
+        getItems({ options = {}, params = {} } = {}) {
+            return [
+                {
+                    id: 1,
+                    name: "John",
+                    car: "Mercedes Benz"
+                },
+                {
+                    id: 2,
+                    name: "Anna",
+                    car: "Volvo"
+                },
+                {
+                    id: 3,
+                    name: "Jorge",
+                    car: "Renault"
+                }
+            ];
+        }
     },
     template: `
-    <div class="filter-platforme">
-    <search-platforme placeholder="Search Orders"   />
-            <table-platforme
-                class="table"
-                v-bind:columns="filterColumns"
-                v-bind:items="filterItems"
-            >
-                    <template v-slot="{ item }">
-                        <td class="id">
-                            {{ item.id }}
-                        </td>
-                        <td class="user">
-                            {{ item.name || "-" }}
-                        </td>
-                        <td class="device">
-                            {{ item.car }}
-                        </td>
-                    </template>
-            </table-platforme>
-        </div>
+    <div>
+      <filter-platforme 
+      v-bind:getItems="getItems"
+      v-bind:columns="filterColumns"
+      v-bind:filter="filter"
+      >
+      <template v-slot:item="{ item, index, addFilter }">
+        <td class="id">
+            {{ item.id }}
+        </td>
+        <td class="user" v-on:click="addFilter('user', item.user)">
+            {{ item.name || "-" }}
+        </td>
+        <td class="device" v-on:click="addFilter('device', item.device)">
+            {{ item.car }}
+        </td>
+        </template>
+      </filter-platforme>
+    </div>
+
     `
 }));
