45db5fea410ab1e4a96b3ea5ce10c07a6cc68846
Daniel Blazeque <v-db@platforme.com>
2019-11-15 14:40:39 +0000

    table working

--- vue/components/ui/filter-platforme/filter-platforme.stories.js
+++ vue/components/ui/filter-platforme/filter-platforme.stories.js
@@ -1,8 +1,57 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
 
-storiesOf("Input", module)
-    .addDecorator(withKnobs)
-    .add("Filter", () => ({
-        template: "<filter-platforme />"
-    }));
+storiesOf("Input", module).add("Filter", () => ({
+    props: {
+        filterColumns: {
+            type: Array,
+            default: () => [
+                { id: "id", title: "ID" },
+                { id: "name", title: "Name" },
+                { id: "car", title: "Car" }
+            ]
+        }
+    },
+    data: function() {
+        return {
+            filterItems: [
+                {
+                    id: 1,
+                    name: "John",
+                    car: "Mercedes-Benz"
+                },
+                {
+                    id: 2,
+                    name: "Marie",
+                    car: "Volvo"
+                },
+                {
+                    id: 3,
+                    name: "Anna",
+                    car: "Land-Rover"
+                }
+            ]
+        };
+    },
+    template: `
+    <div class="filter-platforme">
+    <search-platforme placeholder="Search Orders"   />
+            <table-platforme
+                class="table"
+                v-bind:columns="filterColumns"
+                v-bind:items="filterItems"
+            >
+                    <template v-slot="{ item }">
+                        <td class="id">
+                            {{ item.id }}
+                        </td>
+                        <td class="user">
+                            {{ item.name || "-" }}
+                        </td>
+                        <td class="device">
+                            {{ item.car }}
+                        </td>
+                    </template>
+            </table-platforme>
+        </div>
+    `
+}));
