02e44792b85cde083779e3940f413189fd23592e
3rdvision <kazgru@gmail.com>
2019-11-21 18:28:38 +0000

    Set of UI and CSS fixes

--- vue/components/ui/footer-platforme/footer-platforme.vue
+++ vue/components/ui/footer-platforme/footer-platforme.vue
@@ -1,17 +1,6 @@
 <template>
     <div class="footer-platforme">
-        <ul class="links-left">
-            <li class="desktop copy">
-                © 2019 Platforme International
-            </li>
-            <li>
-                <link-platforme text="Terms" href="https://www.platforme.com/terms" />
-            </li>
-            <li>
-                <link-platforme text="Privacy" href="https://www.platforme.com/privacy" />
-            </li>
-        </ul>
-        <ul class="links-right">
+        <ul class="links-part links-left">
             <li>
                 <link-platforme text="Contact Platforme" href="mailto:info@platforme.com" />
             </li>
@@ -22,21 +11,29 @@
             <li>
                 <link-platforme text="Training" href="https://docs.platforme.com" target="_blank" />
             </li>
+        </ul>
+        <ul class="links-part links-right">
             <li>
                 <link-platforme text="DevPortal" href="https://dev.platforme.com" target="_blank" />
             </li>
             <li>
                 <link-platforme text="About" href="https://www.platforme.com" target="_blank" />
             </li>
+            <li>
+                <link-platforme text="Terms" href="https://www.platforme.com/terms" />
+            </li>
+            <li>
+                <link-platforme text="Privacy" href="https://www.platforme.com/privacy" />
+            </li>
         </ul>
         <div class="links-logo">
-            <span class="mobile foot-left copy">
-                © 2019 Platforme International
-            </span>
-            <span class="footer-logo foot-right">
+            <span class="footer-logo">
                 <a href="https://www.platforme.com" target="_blank" class="no-style">
                     <img src="~./assets/platforme.svg" />
                 </a>
+            </span>
+            <span class="footer-text">
+                © 2019 Platforme International
             </span>
         </div>
     </div>
@@ -45,176 +42,121 @@
 <style lang="scss" scoped>
 @import "css/variables.scss";
 
-.mobile {
-    display: none;
-}
-
-body.mobile .desktop {
-    display: none;
-}
-
-body.mobile .container-links {
-    display: block;
-    margin: 15px 15px 0px 15px;
-}
-
 .footer-platforme {
     border-top: 1px solid #f0f0f0;
     margin: 64px auto 0px auto;
     max-width: 1240px;
-    padding-bottom: 20px;
+    padding: 0px 15px 20px 15px;
 }
 
+body.tablet .footer-platforme,
 body.mobile .footer-platforme {
-    border-top: 1px solid #e4e8f0;
-    margin: 0px auto 0px auto;
+    border: none;
+    font-size: 0px;
+    margin-top: 35px;
+    padding: 0px 20px 50px 30px;
     padding-bottom: 50px;
 }
 
-ul {
+.footer-platforme .links-part {
     display: inline-block;
+    line-height: 60px;
     list-style: none;
     margin: 0px 0px 0px 0px;
+    overflow: visible;
     padding: 0px 0px 0px 0px;
+    width: calc(50% - 120px);
     width: auto;
 }
 
-ul li {
-    display: inline;
-    font-size: 12px;
+body.tablet .footer-platforme .links-part,
+body.mobile .footer-platforme .links-part {
+    display: inline-block;
+    float: none;
+    line-height: 17px;
+    width: 50%;
 }
 
-body.mobile ul li {
+.footer-platforme .links-part.links-left {
+    float: left;
+    text-align: left;
+}
+
+.footer-platforme .links-part.links-right {
+    float: right;
+    text-align: right;
+}
+
+body.mobile .footer-platforme .links-part.links-right {
+    text-align: left;
+}
+
+.footer-platforme .links-part > li {
+    display: inline;
+    font-size: 12px;
+    margin-right: 12px;
+}
+
+.footer-platforme .links-part > li:last-child {
+    margin-right: 0px;
+}
+
+.footer-platforme .links-part > li:hover {
+    background-color: transparent;
+}
+
+body.tablet .footer-platforme .links-part > li,
+body.mobile .footer-platforme .links-part > li {
     display: block;
     font-size: 14px;
     letter-spacing: 0.7px;
     margin: 20px 0px 0px 0px;
 }
 
-ul li.copy {
-    color: #6d6d6d;
-    font-size: 11px;
-    font-weight: 600;
-}
-
-body.mobile ul li.copy {
-    color: #999999;
-    font-size: 14px;
-    font-weight: 700;
-    letter-spacing: 0.7px;
-    line-height: 17px;
-}
-
-ul li:hover {
-    background-color: transparent;
-}
-
-.links-left,
-.links-right {
-    line-height: 60px;
-    overflow: visible;
-    width: calc(50% - 35px);
-}
-
-body.mobile .links-left,
-body.mobile .links-right {
-    display: inline-block;
-    float: none;
-    line-height: 17px;
-    overflow: visible;
-    text-align: left;
-    vertical-align: top;
-    width: calc(50% - 10px);
-}
-
-.links-left {
-    float: left;
-    text-align: left;
-}
-
-body.mobile .links-left {
-    margin-left: 15px;
-}
-
-.links-right {
-    float: right;
-    text-align: right;
-}
-
-.links-left li {
-    margin-right: 12px;
-}
-
-.links-right li {
-    margin-left: 12px;
-}
-
-.links-logo {
+.footer-platforme .links-logo {
     height: 60px;
     line-height: 60px;
+    text-align: center;
 }
 
-body.mobile .links-logo {
-    display: block;
-    height: 70px;
-    line-height: 70px;
-    margin: 5px 0px 0px 0px;
-    text-align: justify;
+body.tablet .footer-platforme .links-logo,
+body.mobile .footer-platforme .links-logo {
+    text-align: left;
 }
 
-body.mobile .links-logo::after {
-    content: "";
+.footer-platforme .links-logo .footer-text {
+    color: #999999;
+    font-size: 11px;
+    font-weight: 600;
+    letter-spacing: 0.7px;
+    margin-left: 16px;
+}
+
+body.tablet .footer-platforme .links-logo .footer-text,
+body.mobile .footer-platforme .links-logo .footer-text {
+    font-size: 14px;
+    line-height: initial;
+    margin-left: 0px;
+    vertical-align: bottom;
+}
+
+.footer-platforme .links-logo .footer-logo {
+    cursor: pointer;
     display: inline-block;
-    width: 100%;
-}
-
-body.mobile .foot-left,
-body.mobile .foot-right {
-    display: inline-block;
-}
-
-body.mobile .foot-left {
-    margin: 0px 0px 0px 16px;
-}
-
-body.mobile .foot-right {
-    margin: 0px 16px 0px 0px;
-}
-
-.footer-logo {
     height: 28px;
-    margin: 0px 0px 0px 0px;
     vertical-align: middle;
     width: 28px;
 }
 
-body.mobile .footer-logo {
+body.tablet .footer-platforme .links-logo .footer-logo,
+body.mobile .footer-platforme .links-logo .footer-logo {
+    float: right;
     height: 60px;
-    vertical-align: baseline;
     width: 60px;
 }
 
-.footer-logo > a {
-    border-bottom: none;
-    padding-bottom: 0px;
-}
-
-.footer-logo > a:hover,
-.footer-logo > a.hover {
-    border-color: transparent;
-}
-
-.footer-logo > a > img {
+.footer-platforme .links-logo .footer-logo > a > img {
     display: block;
-}
-
-.footer-logo,
-.footer-logo:not(.button-no-style) {
-    background: transparent none no-repeat left 0px;
-    border-bottom: none;
-    cursor: pointer;
-    display: inline-block;
-    padding: 0px 0px 0px 0px;
 }
 </style>
 
