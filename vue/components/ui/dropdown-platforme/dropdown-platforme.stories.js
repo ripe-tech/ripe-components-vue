fedde3732047e88a8d4bdaa5025cc0d832e2d9ae
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-02 11:10:17 +0000

    Redoing dropdown-platforme

--- vue/components/ui/dropdown-platforme/dropdown-platforme.stories.js
+++ vue/components/ui/dropdown-platforme/dropdown-platforme.stories.js
@@ -1,82 +1,38 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 
 storiesOf("Dropdown", module)
     .addDecorator(withKnobs)
-    .add("Visible", () => ({
+    .add("Dropdown", () => ({
         props: {
-            visible: {
-                default: boolean("Visible", true)
-            }
         },
         data: function() {
             return {
-                items: [
+                options: [
                     {
-                        id: "text_1",
-                        text: "Text 1"
+                        id: "option_1",
+                        text: "Option 1"
                     },
                     {
-                        id: "text_2",
-                        text: "Text 2"
+                        id: "option_2",
+                        text: "Option 2"
                     },
                     {
-                        id: "text_3",
-                        text: "Text 3"
+                        id: "option_3",
+                        text: "Option 3"
                     },
                     {
-                        id: "text_platforme",
-                        text: "Text Platforme",
-                        href: "https://www.platforme.com"
+                        id: "option_4",
+                        text: "Option 4"
                     },
                     {
-                        id: "text_platforme_blank",
-                        text: "Text Platforme Blank",
-                        href: "https://www.platforme.com",
-                        target: "_blank"
+                        id: "option_5",
+                        text: "Option 5"
                     }
                 ]
             };
         },
         template: `
-            <div style="max-width: 200px">
-                <dropdown-platforme v-bind:items="items" v-bind:visible="visible"></dropdown-platforme>
-            </div>
-        `
-    }))
-    .add("Custom", () => ({
-        props: {
-            visible: {
-                default: boolean("Visible", true)
-            }
-        },
-        data: function() {
-            return {
-                items: [
-                    {
-                        id: "text_1",
-                        text: "Text 1"
-                    },
-                    {
-                        id: "text_2",
-                        text: "Text 2",
-                        subtext: "Subtext"
-                    },
-                    {
-                        id: "text_3",
-                        text: "Text 3"
-                    }
-                ]
-            };
-        },
-        template: `
-            <div style="max-width: 200px">
-                <dropdown-platforme v-bind:items="items" v-bind:visible="visible">
-                    <template v-slot="{ item: { id, text, subtext } }">
-                        <b>{{ text }}</b>
-                        <pre v-if="subtext">{{ subtext }}</pre>
-                    </template>
-                </dropdown-platforme>
-            </div>
-        `
+                <dropdown-platforme v-bind:options="options" />
+                `
     }));
