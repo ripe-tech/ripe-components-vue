c4b02f876471e6f1e81cb7c375f57cd813724c2e
Mauro Benedito <v-mb@platforme.com>
2019-12-03 10:22:15 +0000

    renaming const

--- vue/mixins/device.js
+++ vue/mixins/device.js
@@ -7,7 +7,7 @@ const MOBILE_WIDTH = 420;
 const DESKTOP_DEVICE_CLASS = "desktop-device";
 const TABLET_DEVICE_CLASS = "tablet-device";
 const MOBILE_DEVICE_CLASS = "mobile-device";
-const TABLET_REGEX = new RegExp(
+const TABLET_DEVICE_REGEX = new RegExp(
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk/i
 );
 const MOBILE_DEVICE_PREFIX_REGEX = new RegExp(
@@ -83,7 +83,7 @@ const deviceMixin = {
         },
         _isTabletDevice(data) {
             const prefix = data.substr(0, 4);
-            const tablet = TABLET_REGEX.test(data);
+            const tablet = TABLET_DEVICE_REGEX.test(data);
             const tabletPrefix = MOBILE_DEVICE_PREFIX_REGEX.test(prefix);
             const isTablet = tablet || Boolean(tabletPrefix);
             return isTablet && !this._isMobileDevice(data);
