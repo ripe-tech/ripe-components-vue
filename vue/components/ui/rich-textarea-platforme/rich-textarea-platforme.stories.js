edef196008916e451b4ffe1b0ef587eb527a030f
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-10 16:45:04 +0000

    rich-textarea-platforme initial commit !

---
+++ vue/components/ui/rich-textarea-platforme/rich-textarea-platforme.stories.js
@@ -0,0 +1,19 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, text } from "@storybook/addon-knobs";
+
+storiesOf("Input", module)
+    .addDecorator(withKnobs)
+    .add("Rich Text Area", () => ({
+        props: {
+            hello: {
+                default: text("Hello World", "Hello !!!")
+            }
+        },
+        template: `
+            <div>
+                <rich-textarea-platforme
+                    v-bind:hello="hello"
+                />
+            </div>
+            `
+    }));
---
+++ vue/components/ui/rich-textarea-platforme/rich-textarea-platforme.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>{{ hello }}</h1>
+</template>
+
+<style lang="scss" scoped>
+@import "css/variables.scss";
+
+</style>
+
+<script>
+export const RichTextareaPlatforme = {
+    name: "rich-textarea-platforme",
+    props: {
+        hello: {
+            type: String,
+            default: null
+        }
+    }
+};
+
+export default RichTextareaPlatforme;
+</script>
