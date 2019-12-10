11a3b00ca9010b5cc28e9b7ac25d112e61d28e66
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-10 16:56:47 +0000

    Text area showing with options correctly positioned

--- vue/components/ui/rich-textarea-platforme/rich-textarea-platforme.vue
+++ vue/components/ui/rich-textarea-platforme/rich-textarea-platforme.vue
@@ -1,9 +1,40 @@
 <template>
-    <h1>{{ hello }}</h1>
+        <div class="rich-textarea">
+            <textarea-platforme
+                v-bind:placeholder="'Say something here...'"
+                v-bind:resize="false"
+                v-on:update:value="value => onTextareaValue(value)"
+            />
+            <div class="options">
+                <div class="optionExample" />
+                <div class="optionExample" />
+                <div class="optionExample" />
+                <div class="optionExample" />
+                <div class="optionExample" />
+            </div>
+        </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
+
+
+
+.rich-textarea .options {
+    font-size: 0px; //It fixes white spaces between divs
+    //background-color: lightseagreen;
+    margin-top: -31px;
+    padding: 0px 9px 0px 9px;
+}
+
+.rich-textarea .optionExample {
+    background: url("~./assets/clip.svg") center;
+    background-size: 22px 22px;
+    display: inline-block;
+    height: 22px;
+    margin: 4px 4px 4px 4px;
+    width: 22px;
+}
 
 </style>
 
