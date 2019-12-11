037db146aacad6f13a704ed21a61875f3cabed48
3rdvision <kazgru@gmail.com>
2019-12-11 12:27:43 +0000

    Refactored avatar label to include avatar and no component

--- vue/components/ui/avatar-label-platforme/avatar-label-platforme.vue
+++ vue/components/ui/avatar-label-platforme/avatar-label-platforme.vue
@@ -1,36 +1,209 @@
 <template>
-    <div class="avatar-label" v-bind:class="[size]">
-        <slot name="avatar">
-            <avatar-platforme v-bind:size="size" v-bind:imgUrl="imgUrl"></avatar-platforme>
-        </slot>
-            <span>
-                <label-platforme>Option</label-platforme>
-                <label-platforme>Subtext</label-platforme>
-            </span>
-            <label-platforme>Metadata</label-platforme>
+    <div class="avatar-container" v-bind:class="[size]">
+        <div class="avatar" v-bind:class="[{ notify: notify }, size]">
+            <img class="image" v-bind:src="imgUrl" alt="avatar" />
+            <slot name="notify-dot">
+                <div class="dot"
+                v-bind:style="{ 'border-color': notifyBorderColor, background: notifyColor }"/>
+            </slot>
+        </div>
+        <div class="text left" v-if="title || subtitle" v-bind:class="[size]">
+            <label-platforme v-if="title" class="title">{{title}}</label-platforme>
+            <label-platforme v-if="subtitle" class="subtitle">{{subtitle}}</label-platforme>
+        </div>
+        <div class="text right" v-bind:class="[size]">
+            <label-platforme v-if="content" class="content">{{content}}</label-platforme>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
 
-span {
+.avatar-container{
+    font-size: 0px;
     display: inline-block;
 }
 
-span > * {
-    display: block;
+
+.avatar {
+    display: inline-block;
+    position: relative;
+    font-size: 0px;
+}
+
+.avatar .image {
+    border-radius: 50%;
+    height: auto;
+}
+
+.avatar.giant .image {
+    height: 128px;
+    width: 128px;
+}
+
+.avatar.giant .dot {
+    height: 17px;
+    right: 9px;
+    top: 8px;
+    width: 17px;
+}
+
+.avatar.large .image {
+    height: 96px;
+    width: 96px;
+}
+
+.avatar.large .dot {
+    height: 12px;
+    right: 6px;
+    top: 7px;
+    width: 12px;
+}
+
+.avatar.medium .image {
+    height: 40px;
+    width: 40px;
+}
+
+.avatar.medium .dot {
+    border-width: 0.9px;
+    height: 6px;
+    right: 3px;
+    top: 1px;
+    width: 6px;
+}
+
+.avatar.small .image {
+    height: 32px;
+    width: 32px;
+}
+
+.avatar.small .dot {
+    border-width: 0.9px;
+    height: 4px;
+    right: 3px;
+    top: 1px;
+    width: 4px;
+}
+
+.avatar.tiny .image {
+    height: 24px;
+    width: 24px;
+}
+
+.avatar.tiny .dot {
+    border-width: 0.3px;
+    height: 3px;
+    right: 2px;
+    top: 1px;
+    width: 3px;
+}
+
+.avatar.micro .image {
+    height: 16px;
+    width: 16px;
+}
+
+.avatar.micro .dot {
+    border-width: 0.1px;
+    height: 2px;
+    right: 1px;
+    top: 0px;
+    width: 2px;
+}
+
+.avatar .dot {
+    background: #4b8dd7;
+    border: 1px solid #ffffff;
+    border-radius: 50%;
+    height: 10%;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    width: 10%;
+}
+
+.avatar:not(.notify) .dot {
+    display: none;
+}
+
+.text {
+    display: inline-flex;
+    justify-content: space-evenly;
+    vertical-align: top;
+    flex-direction: column;
+    padding: 0px 8px 0px 8px;
+}
+
+.text.giant{
+    height: 128px;
+}
+
+.text.large{
+    height: 96px;
+}
+
+.text.medium{
+    height: 40px;
+}
+
+.text.small{
+    height: 32px;
+}
+
+.text.tiny{
+    height: 24px;
+}
+
+.text.micro{
+    height: 16px;
+}
+
+.text .title ::v-deep {
+    font-size: 14px;
+    font-weight: 700;
+    color: $dark;
+}
+.text .subtitle ::v-deep {
+    font-weight: 500;
+    letter-spacing: 0.3px;
+    color: $dark;
+}
+
+.text.right {
+    padding: 0px 0px 0px 0px;
+}
+
+.text:empty{
+    display: none;
 }
 
 </style>
 
 <script>
 export const AvatarLabelPlatforme = {
-    name: "avatar-label-platforme",
+    name: "avatar-container-platforme",
     props: {
         size: {
             type: String,
             default: "small"
+        },
+        title: {
+            type: String,
+            default: null
+        },
+        subtitle: {
+            type: String,
+            default: null
+        },
+        content: {
+            type: String,
+            default: null
+        },
+        subtitle: {
+            type: String,
+            default: null
         },
         imgUrl: {
             type: String,
@@ -41,6 +214,10 @@ export const AvatarLabelPlatforme = {
             default: false
         },
         notifyColor: {
+            type: String,
+            default: null
+        },
+        notifyBorderColor: {
             type: String,
             default: null
         }
