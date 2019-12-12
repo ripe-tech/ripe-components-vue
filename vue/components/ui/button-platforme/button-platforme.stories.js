02eab957a636f6edab633cff4e7ca1be0ea25802
joamag <joamag@gmail.com>
2019-12-12 11:02:56 +0000

    Naming for Button Platforme

--- vue/components/ui/button-platforme/button-platforme.stories.js
+++ vue/components/ui/button-platforme/button-platforme.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 
-storiesOf("Button", module)
+storiesOf("Button Platforme", module)
     .addDecorator(withKnobs)
     .add("Platforme ID", () => ({
         props: {
