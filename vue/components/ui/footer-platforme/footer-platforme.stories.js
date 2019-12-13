62b2dfed1b03261cfb41b80e57c325415d57b1d7
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-13 14:57:01 +0000

    Fixed merge

---
+++ vue/components/ui/footer-platforme/footer-platforme.stories.js
@@ -0,0 +1,5 @@
+import { storiesOf } from "@storybook/vue";
+
+storiesOf("Footer", module).add("Platforme", () => ({
+    template: "<div><global></global><footer-platforme></footer-platforme></div>"
+}));
