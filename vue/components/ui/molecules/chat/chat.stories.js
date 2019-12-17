cb9e8664e2dcae2a4dc3683d98966d53c099630d
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-17 15:03:46 +0000

    Fixed chat imports

--- vue/components/ui/molecules/chat/chat.stories.js
+++ vue/components/ui/molecules/chat/chat.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from "@storybook/vue";
 import { withKnobs } from "@storybook/addon-knobs";
 
-storiesOf("Chat", module)
+storiesOf("Molecules", module)
     .addDecorator(withKnobs)
     .add("Chat", () => ({
         props: {
