8078749ffbdf7dba6a547018f22d17e6cd2103a5
3rdvision <kazgru@gmail.com>
2019-12-20 10:54:35 +0000

    Make table props reactive

--- vue/components/ui/molecules/table/table.stories.js
+++ vue/components/ui/molecules/table/table.stories.js
@@ -1,42 +1,77 @@
 import { storiesOf } from "@storybook/vue";
+import { withKnobs, select, text, number, boolean } from "@storybook/addon-knobs";
 
-storiesOf("Molecules", module).add("Table", () => ({
-    props: {
-        mockColumns: {
-            type: Array,
-            default: () => [
-                { id: "id", title: "ID" },
-                { id: "user", title: "User" },
-                { id: "system", title: "System" }
-            ]
-        }
-    },
-    data: function() {
-        return {
-            mockItems: [
-                {
-                    id: 1,
-                    user: "Bill Gates",
-                    system: "Windows"
-                },
-                {
-                    id: 2,
-                    user: "Steve Jobs",
-                    system: "Macintosh"
-                },
-                {
-                    id: 3,
-                    user: "Linus Torvalds",
-                    system: "Linux"
-                }
-            ]
-        };
-    },
-    template: `
-    <table-ripe
-        class="table"
-        v-bind:columns="mockColumns"
-        v-bind:items="mockItems"
-    />
-    `
-}));
+storiesOf("Molecules", module)
+    .addDecorator(withKnobs)
+    .add("Table", () => ({
+        props: {
+            mockItems: {
+                default: () => [
+                    {
+                        id: 1,
+                        user: "Bill Gates",
+                        system: "Windows"
+                    },
+                    {
+                        id: 2,
+                        user: "Steve Jobs",
+                        system: "Macintosh"
+                    },
+                    {
+                        id: 3,
+                        user: "Linus Torvalds",
+                        system: "Linux"
+                    }
+                ]
+            },
+            mockColumns: {
+                type: Array,
+                default: () => [
+                    { id: "id", title: "ID" },
+                    { id: "user", title: "User" },
+                    { id: "system", title: "System" }
+                ]
+            },
+            sort: {
+                default: select(
+                    "Sort Column",
+                    {
+                        Unset: null,
+                        Id: "id",
+                        User: "user",
+                        System: "system"
+                    },
+                    null
+                )
+            },
+            reverse: {
+                default: () => boolean("Reverse", null)
+            }
+        },
+        data: function() {
+            return {
+                reverseData: this.reverse,
+                sortData: this.sort
+            };
+        },
+        watch: {
+            sort(value) {
+                this.sortData = value;
+            },
+            reverse(value) {
+                this.reverseData = value;
+            }
+        },
+        template: `
+            <div>
+                <table-ripe
+                    class="table"
+                    v-bind:columns="mockColumns"
+                    v-bind:items="mockItems"
+                    v-bind:sort.sync="sortData"
+                    v-bind:reverse.sync="reverseData"
+                />
+                <p>Sort: {{ sortData }}, Reverse: {{ reverseData }}</p>
+            </div>
+        `
+    }));
