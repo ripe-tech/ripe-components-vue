f526a2938189bb10d09336d810d3243f5d34f5ef
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-23 10:47:13 +0000

    "chat-message" into "chat" (#119)
    * "chat-message-platforme" initial commit
    
    * Chat message style
    
    * Fixed positions
    
    * Added message identification information props
    
    * Whitespaces fix
    
    * Added message prop
    
    * Attachments props
    
    * Added temporary reactions
    
    * Added hover effect
    
    * Added extra options
    
    * Fixed line
    
    * Pretty
    
    * Hover showing everything correctly
    
    * Removed unused code
    
    * Options buttons hover
    
    * Options clickable
    
    * Fixed attachments links
    
    * Options always on the right
    
    * Pretty
    
    * Add event counter
    
    * Renaming folders
    
    * Removed "chat" from this branch
    
    * Fixed merge
    
    * Replaced "button-icon" with an "reaction" example
    
    * Add reactions and reaction button
    
    * Small refactors
    
    * Small refactors
    
    * Update with avatar component
    
    Co-authored-by: Daniel Azevedo <kazgru@gmail.com>

--- vue/components/ui/atoms/avatar/avatar.vue
+++ vue/components/ui/atoms/avatar/avatar.vue
@@ -15,58 +15,47 @@
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
-
 .avatar {
     border-radius: 50% 50% 50% 50%;
     display: inline-block;
 }
-
 .avatar.clickable {
     cursor: pointer;
 }
-
 .avatar.clickable:hover {
     background-color: rgba(60, 64, 67, 0.08);
 }
-
 .avatar.clickable:active,
 .avatar.clickable.active {
     background-color: rgba(60, 64, 67, 0.2);
 }
-
 .avatar.tiny.clickable {
     height: 24px;
     padding: 4px 4px 4px 4px;
     width: 24px;
 }
-
 .avatar.small.clickable {
     height: 34px;
     padding: 6px 6px 6px 6px;
     width: 34px;
 }
-
 .avatar.medium.clickable {
     height: 40px;
     padding: 8px 8px 8px 8px;
     width: 40px;
 }
-
 .avatar.large.clickable {
     height: 96px;
     padding: 10px 10px 10px 10px;
     width: 96px;
 }
-
 .avatar .image-container {
     font-size: 0px;
     position: relative;
 }
-
 .avatar .image-container > .image {
     border-radius: 50% 50% 50% 50%;
 }
-
 .avatar .image-container > .dot {
     background: #4b8dd7;
     border: 1px solid #ffffff;
@@ -76,18 +65,15 @@
     right: 0px;
     top: 0px;
 }
-
 .avatar.notify .image-container > .dot {
     display: inline-block;
 }
-
 .avatar.tiny .image-container,
 .avatar.tiny .image-container > .image {
     height: 24px;
     line-height: 24px;
     width: 24px;
 }
-
 .avatar.tiny .image-container > .dot {
     border-width: 0.3px;
     height: 5px;
@@ -95,42 +81,36 @@
     top: 1px;
     width: 5px;
 }
-
 .avatar.small .image-container,
 .avatar.small .image-container > .image {
     height: 34px;
     line-height: 34px;
     width: 34px;
 }
-
 .avatar.small .image-container > .dot {
     height: 7px;
     right: 0px;
     top: 0px;
     width: 7px;
 }
-
 .avatar.medium .image-container,
 .avatar.medium .image-container > .image {
     height: 40px;
     line-height: 40px;
     width: 40px;
 }
-
 .avatar.medium .image-container > .dot {
     height: 8px;
     right: 1px;
     top: 1px;
     width: 8px;
 }
-
 .avatar.large .image-container,
 .avatar.large .image-container > .image {
     height: 96px;
     line-height: 96px;
     width: 96px;
 }
-
 .avatar.large .image-container > .dot {
     border-width: 2px;
     height: 19px;
@@ -179,6 +159,5 @@ export const Avatar = {
         }
     }
 };
-
 export default Avatar;
 </script>
---
+++ vue/components/ui/molecules/chat-message/chat-message.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="chat-message">
+        <avatar class="user-image" v-bind:img-url="userImgPath" v-bind:size="'tiny'" />
+        <div class="message-container">
+            <div class="message-header">
+                <div class="user-information">
+                    <span class="username">{{ username }}</span>
+                    <span class="date">{{ timeString(date) }}</span>
+                </div>
+                <div class="line-container" v-if="hasReactionOptions">
+                    <hr />
+                </div>
+            </div>
+            <div class="message-content">
+                <span class="message">{{ message }}</span>
+                <div class="links">
+                    <link-ripe
+                        v-bind:text="attachment.name"
+                        v-bind:href="attachment.path"
+                        v-bind:disabled="false"
+                        v-bind:size="'small'"
+                        v-bind:color="'blue'"
+                        v-for="attachment in attachments"
+                        v-bind:key="attachment.name"
+                    />
+                </div>
+                <div class="reactions">
+                    <reaction
+                        v-bind:icon="reaction.icon"
+                        v-bind:img-url="reaction.imgUrl"
+                        v-bind:emoji="reaction.emoji"
+                        v-bind:count.sync="reaction.count"
+                        v-bind:user-reacted.sync="reaction.userHasReacted"
+                        v-bind:behavior="true"
+                        v-for="reaction in reactions"
+                        v-bind:key="reaction.id"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="options-container" v-if="hasReactionOptions">
+            <div class="reaction-option" v-on:click="onReactionOptionClick('reactionOption')" />
+            <div class="more-options-option" v-on:click="onMoreOptionsOptionClick('moreOptions')" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "css/variables.scss";
+
+.chat-message {
+    background-color: $grey-6;
+    display: flex;
+    padding: 6px 0px 6px 0px;
+}
+
+.chat-message .user-image {
+    border: solid 1px #e3e8f1;
+    border-radius: 50%;
+    box-shadow: 0 2px 5px 0 rgba(67, 86, 100, 0.25);
+    height: 24px;
+    margin: 0px 8px 0px 0px;
+    width: 24px;
+}
+
+.chat-message .message-container {
+    display: inline-block;
+    flex: 1;
+}
+
+.chat-message .message-container .message-header {
+    display: flex;
+    margin: 3px 0px 0px 0px;
+}
+
+.chat-message .message-container .message-header .user-information {
+    display: inline-block;
+}
+
+.chat-message .message-container .message-header .user-information .username {
+    color: #0d0d0d;
+    font-weight: bold;
+    letter-spacing: 0.7px;
+    padding: 0px 6px 0px 0px;
+}
+
+.chat-message .message-container .message-header .user-information .date {
+    color: #a4adb5;
+    font-size: 12px;
+    letter-spacing: 0.3px;
+}
+
+.chat-message .message-container .message-header .line-container {
+    flex: 1 0;
+    margin: 2px 0px 0px 20px;
+    visibility: hidden;
+}
+
+.chat-message .message-container .message-header .line-container hr {
+    border: solid 1px #e4e8f0;
+}
+
+.chat-message .message-container .message-content .message {
+    color: #0d0d0d;
+    letter-spacing: 0.3px;
+}
+
+.chat-message .message-container .message-content .links {
+    margin: 6px 0px 0px 0px;
+}
+
+.chat-message .message-container .message-content .links .link {
+    display: table;
+}
+
+.chat-message .message-container .message-content .reactions {
+    font-size: 0px; //It fixes white spaces between divs
+    margin: 10px 0px 0px 0px;
+}
+
+.chat-message .message-container .message-content .reactions > .reaction {
+    margin: 0px 2px 0px 0px;
+}
+
+.chat-message .options-container {
+    background-color: $grey-6;
+    border: solid 1px #e4e8f0;
+    border-radius: 6px;
+    display: inline-flex;
+    font-size: 0px;
+    height: 26px;
+    padding: 2px 2px 2px 2px;
+    visibility: hidden;
+}
+
+.chat-message .options-container .reaction-option,
+.chat-message .options-container .more-options-option {
+    cursor: pointer;
+    height: 24px;
+    opacity: 0.4;
+    width: 30px;
+}
+
+.chat-message .options-container .reaction-option {
+    background: url("~./assets/happy-face.svg") center no-repeat;
+    background-size: 18px 18px;
+    border-right: solid 1px #e4e8f0;
+}
+
+.chat-message .options-container .more-options-option {
+    background: url("~./assets/ellypsis.svg") center no-repeat;
+    background-size: 18px 18px;
+    border-left: solid 1px #e4e8f0;
+}
+
+.chat-message:hover,
+.chat-message:hover .options-container,
+.chat-message:hover .message-container .message-header .line-container {
+    background-color: $white;
+    visibility: visible;
+}
+
+.chat-message .options-container .reaction-option:hover,
+.chat-message .options-container .more-options-option:hover {
+    opacity: 1;
+}
+
+.chat-message .options-container:hover {
+    border: solid 3px #e4e8f0;
+    padding: 0px 0px 0px 0px;
+}
+</style>
+
+<script>
+import { utilsMixin } from "../../../../mixins";
+
+export const ChatMessage = {
+    name: "chat-message",
+    mixins: [utilsMixin],
+    props: {
+        userImgPath: {
+            // TODO remove -> this will be replaced by avatar
+            type: String,
+            default: "http://i.pravatar.cc"
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Number,
+            required: true
+        },
+        message: {
+            type: String,
+            required: true
+        },
+        attachments: {
+            type: Array,
+            default: () => []
+        },
+        reactions: {
+            type: Array,
+            default: () => []
+        },
+        hasReactionOptions: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        emitOptionClick(option) {
+            this.$emit("option-clicked", option);
+        },
+        onReactionOptionClick(option) {
+            this.emitOptionClick(option);
+        },
+        onMoreOptionsOptionClick(option) {
+            this.emitOptionClick(option);
+        }
+    }
+};
+export default ChatMessage;
+</script>
