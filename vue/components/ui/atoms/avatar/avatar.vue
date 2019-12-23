ababecea0d22aa8255cea489f33ffab50a7578d8
3rdvision <kazgru@gmail.com>
2019-12-23 11:59:37 +0000

    revert avatar

--- vue/components/ui/atoms/avatar/avatar.vue
+++ vue/components/ui/atoms/avatar/avatar.vue
@@ -1,23 +1,14 @@
 <template>
-    <div class="avatar" v-bind:class="[{ notify: notify }, size]">
-        <div class="image-container" v-if="hasImg">
-            <img class="image" v-bind:src="imgUrl" alt="avatar" />
+    <div
+        class="avatar"
+        v-bind:class="[{ notify: notify, clickable: clickable, active: active }, size]"
+        v-if="hasImage"
+    >
+        <div class="image-container">
+            <image-ripe v-bind:src="src" v-bind:alt="alt" v-bind:fade="fade" />
             <slot name="dot">
                 <div class="dot" />
             </slot>
-        </div>
-        <div class="text-left" v-if="title || subtitle">
-            <div class="title" v-if="title">
-                {{ title }}
-            </div>
-            <div class="subtitle" v-if="subtitle">
-                {{ subtitle }}
-            </div>
-        </div>
-        <div class="text-right" v-if="content">
-            <div class="content" v-if="content">
-                {{ content }}
-            </div>
         </div>
     </div>
 </template>
@@ -26,13 +17,48 @@
 @import "css/variables.scss";
 
 .avatar {
+    border-radius: 50% 50% 50% 50%;
     display: inline-block;
-    font-size: 0px;
-    white-space: nowrap;
+}
+
+.avatar.clickable {
+    cursor: pointer;
+}
+
+.avatar.clickable:hover {
+    background-color: rgba(60, 64, 67, 0.08);
+}
+
+.avatar.clickable:active,
+.avatar.clickable.active {
+    background-color: rgba(60, 64, 67, 0.2);
+}
+
+.avatar.tiny.clickable {
+    height: 24px;
+    padding: 4px 4px 4px 4px;
+    width: 24px;
+}
+
+.avatar.small.clickable {
+    height: 34px;
+    padding: 6px 6px 6px 6px;
+    width: 34px;
+}
+
+.avatar.medium.clickable {
+    height: 40px;
+    padding: 8px 8px 8px 8px;
+    width: 40px;
+}
+
+.avatar.large.clickable {
+    height: 96px;
+    padding: 10px 10px 10px 10px;
+    width: 96px;
 }
 
 .avatar .image-container {
-    display: inline-block;
     font-size: 0px;
     position: relative;
 }
@@ -55,33 +81,25 @@
     display: inline-block;
 }
 
-.avatar.large .image-container > .image {
-    height: 96px;
-    width: 96px;
+.avatar.tiny .image-container,
+.avatar.tiny .image-container > .image {
+    height: 24px;
+    line-height: 24px;
+    width: 24px;
 }
 
-.avatar.large .image-container > .dot {
-    border-width: 2px;
-    height: 19px;
-    right: 2px;
-    top: 2px;
-    width: 19px;
-}
-
-.avatar.medium .image-container > .image {
-    height: 40px;
-    width: 40px;
-}
-
-.avatar.medium .image-container > .dot {
-    height: 8px;
+.avatar.tiny .image-container > .dot {
+    border-width: 0.3px;
+    height: 5px;
     right: 1px;
     top: 1px;
-    width: 8px;
+    width: 5px;
 }
 
+.avatar.small .image-container,
 .avatar.small .image-container > .image {
     height: 34px;
+    line-height: 34px;
     width: 34px;
 }
 
@@ -92,93 +110,33 @@
     width: 7px;
 }
 
-.avatar.tiny .image-container .image {
-    height: 24px;
-    width: 24px;
+.avatar.medium .image-container,
+.avatar.medium .image-container > .image {
+    height: 40px;
+    line-height: 40px;
+    width: 40px;
 }
 
-.avatar.tiny .image-container .dot {
-    border-width: 0.3px;
-    height: 5px;
+.avatar.medium .image-container > .dot {
+    height: 8px;
     right: 1px;
     top: 1px;
-    width: 5px;
+    width: 8px;
 }
 
-.avatar .text-left,
-.avatar .text-right {
-    display: inline-flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    vertical-align: top;
-}
-
-.avatar .text-left {
-    padding: 0px 8px 0px 8px;
-}
-
-.avatar .text-right {
-    padding: 0px 8px 0px 0px;
-}
-
-.avatar .text-left .title {
-    color: $dark;
-    font-weight: 700;
-    letter-spacing: 0.5px;
-}
-
-.avatar .text-left .subtitle {
-    color: $dark;
-    font-weight: 500;
-    letter-spacing: 0.5px;
-}
-
-.avatar.large .text-left,
-.avatar.large .text-right {
-    font-size: 38px;
+.avatar.large .image-container,
+.avatar.large .image-container > .image {
     height: 96px;
+    line-height: 96px;
+    width: 96px;
 }
 
-.avatar.medium .text-left,
-.avatar.medium .text-right {
-    font-size: 18px;
-    height: 40px;
-}
-
-.avatar.medium .text-left {
-    padding: 0px 7px 0px 7px;
-}
-
-.avatar.medium .text-right {
-    padding: 0px 7px 0px 0px;
-}
-
-.avatar.small .text-left,
-.avatar.small .text-right {
-    font-size: 14px;
-    height: 34px;
-}
-
-.avatar.small .text-left {
-    padding: 0px 6px 0px 6px;
-}
-
-.avatar.small .text-right {
-    padding: 0px 6px 0px 0px;
-}
-
-.avatar.tiny .text-left,
-.avatar.tiny .text-right {
-    font-size: 10px;
-    height: 24px;
-}
-
-.avatar.tiny .text-left {
-    padding: 0px 4px 0px 4px;
-}
-
-.avatar.tiny .text-right {
-    padding: 0px 4px 0px 0px;
+.avatar.large .image-container > .dot {
+    border-width: 2px;
+    height: 19px;
+    right: 2px;
+    top: 2px;
+    width: 19px;
 }
 </style>
 
@@ -190,33 +148,34 @@ export const Avatar = {
             type: String,
             default: "small"
         },
-        title: {
+        src: {
             type: String,
             default: null
         },
-        subtitle: {
+        alt: {
             type: String,
-            default: null
+            default: "avatar"
         },
-        content: {
-            type: String,
-            default: null
-        },
-        imgUrl: {
-            type: String,
-            default: null
+        fade: {
+            type: Boolean,
+            default: false
         },
         notify: {
+            type: Boolean,
+            default: false
+        },
+        clickable: {
+            type: Boolean,
+            default: false
+        },
+        active: {
             type: Boolean,
             default: false
         }
     },
     computed: {
-        hasImg() {
-            return Boolean(this.imgUrl);
-        },
-        hasText() {
-            return Boolean(this.title || this.subtitle || this.content);
+        hasImage() {
+            return Boolean(this.src);
         }
     }
 };
