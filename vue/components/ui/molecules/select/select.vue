9d4615a738db53eb1a66c9de48d3c287193a2394
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-27 12:51:29 +0000

    Fixed padding

--- vue/components/ui/molecules/select/select.vue
+++ vue/components/ui/molecules/select/select.vue
@@ -95,7 +95,7 @@ body.tablet-device .select .select-container {
     outline: none;
     overflow: hidden;
     padding-left: 12px;
-    padding-right: 12px;
+    padding-right: 34px;
     text-overflow: ellipsis;
     transition: width 0.2s ease, border-color 0.2s ease, background-color 0.2s ease;
     user-select: none;
