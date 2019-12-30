92eda677957fd32e7766920535e500edc8664e75
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-30 09:11:09 +0000

    Fixed fix

--- vue/components/ui/organisms/index.js
+++ vue/components/ui/organisms/index.js
@@ -1,15 +1,13 @@
 import { Details } from "./details/details.vue";
 import { Header } from "./header/header.vue";
 import { Listing } from "./listing/listing.vue";
-import { SaveFilterModal } from "./listing/save-filter-modal.vue";
 
 const install = Vue => {
     Vue.component("details-ripe", Details);
     Vue.component("header-ripe", Header);
     Vue.component("listing", Listing);
-    Vue.component("save-filter-modal", SaveFilterModal);
 };
 
-export { Details, Header, Listing, SaveFilterModal };
+export { Details, Header, Listing };
 
 export default install;
