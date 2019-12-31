359e747c9d7a7b1438b9be5ce45c56276540b269
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-31 09:36:55 +0000

    Added default content

--- vue/components/ui/molecules/upload-area/upload-area.vue
+++ vue/components/ui/molecules/upload-area/upload-area.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="upload-area">
-        <p> {{ helloText }} </p>
+        <slot name="drag-area-slot">
+            <div class="area-container">
+                <p class="description">Drag & drop your files here or</p>
+                <button-color class="upload-button" v-bind:text="'Upload Files'" v-bind:icon="'cloud'" />
+            </div>
+        </slot>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
+
+.upload-area .area-container {
+    border-radius: 8px;
+    border: dashed 2px $light-white;
+    text-align: center;
+}
+
 </style>
 
 <script>
