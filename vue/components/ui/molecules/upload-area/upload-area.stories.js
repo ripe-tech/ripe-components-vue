112deea98392674b098d6676e264f146f3e30ba4
Nuno Silva <nuno_nfss96@hotmail.com>
2020-01-03 09:39:17 +0000

    Removed custom example toggle

--- vue/components/ui/molecules/upload-area/upload-area.stories.js
+++ vue/components/ui/molecules/upload-area/upload-area.stories.js
@@ -1,14 +1,9 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 
 storiesOf("Molecules", module)
     .addDecorator(withKnobs)
     .add("Upload Area", () => ({
-        props: {
-            customExample: {
-                default: boolean("See custom example", false)
-            }
-        },
         data: function() {
             return {
                 filesData: null
@@ -16,7 +11,8 @@ storiesOf("Molecules", module)
         },
         template: `
             <div>
-                <upload-area v-if="customExample" v-bind:files.sync="filesData">
+                <upload-area v-bind:files.sync="filesData"/>
+                <upload-area v-bind:files.sync="filesData">
                     <template v-slot:drag-area-slot>
                         <div style="height:150px;background-color:aqua;">
                             <p style="font-size:20px;text-align:center">
@@ -25,7 +21,7 @@ storiesOf("Molecules", module)
                         </div>
                     </template>
                 </upload-area>
-                <upload-area v-else v-bind:files.sync="filesData"/>
+                
                 <div>
                     <p>Files:</p>
                     <p v-for="(file, index) in filesData">
