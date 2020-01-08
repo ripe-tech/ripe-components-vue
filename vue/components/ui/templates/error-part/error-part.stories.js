8d60f071e262116f67c49384fb31ff4d14c7b9d4
Gabriel Candal <gabrielcandal@gmail.com>
2020-01-08 17:36:56 +0000

    Add missing stories

---
+++ vue/components/ui/templates/error-part/error-part.stories.js
@@ -0,0 +1,33 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
+storiesOf("Templates", module)
+    .addDecorator(withKnobs)
+    .add("Error", () => ({
+        props: {
+            message: {
+                default: text("Message", "There was an unexpected error")
+            },
+            code: {
+                default: number("Code", null)
+            },
+            loginMessage: {
+                default: text("Login Message", "Try to login again")
+            },
+            image: {
+                default: text("Image", null)
+            },
+            navigation: {
+                default: boolean("Navigation", null)
+            }
+        },
+        template: `
+            <error-part
+                v-bind:message="message"
+                v-bind:code="code"
+                v-bind:loginMessage="loginMessage"
+                v-bind:image="image"
+                v-bind:navigation="navigation"
+            />
+        `
+    }));
