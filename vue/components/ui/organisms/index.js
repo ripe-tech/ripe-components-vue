af4708e63e262e12fbe124f8e8fdb472d68eeaa6
Nuno Silva <nuno_nfss96@hotmail.com>
2020-01-09 16:29:25 +0000

    Cleaning branch

--- vue/components/ui/organisms/index.js
+++ vue/components/ui/organisms/index.js
@@ -1,15 +1,12 @@
-import { Chat } from "./chat/chat.vue";
 import { Details } from "./details/details.vue";
 import { Header } from "./header/header.vue";
 import { Listing } from "./listing/listing.vue";
 
 const install = Vue => {
-    Vue.component("chat", Chat);
     Vue.component("details-ripe", Details);
     Vue.component("header-ripe", Header);
     Vue.component("listing", Listing);
 };
 
-export { Chat, Details, Header, Listing };
 
 export default install;
