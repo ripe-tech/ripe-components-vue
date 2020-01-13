654460c3d721a5ba2e064e334ad0042be644aaed
Gabriel Candal <gcc@platforme.com>
2020-01-13 09:27:16 +0000

    Add table slot

--- vue/components/ui/molecules/table/table.vue
+++ vue/components/ui/molecules/table/table.vue
@@ -21,17 +21,20 @@
             </tr>
         </thead>
         <transition-group tag="tbody" v-bind:name="transition" class="table-body">
-            <tr v-for="(item, index) in sortedItems" v-bind:key="item.id">
-                <slot v-bind:item="item" v-bind:index="index">
-                    <td
-                        v-bind:class="column.value"
-                        v-for="column in columns"
-                        v-bind:key="column.value"
-                    >
-                        {{ item[column.value] }}
-                    </td>
-                </slot>
-            </tr>
+            <template v-for="(item, index) in sortedItems">
+                <tr v-bind:key="item.id" v-on:click="$emit('click', item, index)">
+                    <slot v-bind:item="item" v-bind:index="index">
+                        <td
+                            v-bind:class="column.value"
+                            v-for="column in columns"
+                            v-bind:key="column.value"
+                        >
+                            {{ item[column.value] }}
+                        </td>
+                    </slot>
+                </tr>
+                <slot name="extra-row" v-bind:item="item" v-bind:index="index" />
+            </template>
         </transition-group>
     </table>
 </template>
