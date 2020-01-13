02f20fc610a586e12c3208d2b62c5c84cc2eab67
Nuno Silva <nuno_nfss96@hotmail.com>
2020-01-13 17:00:13 +0000

    Reverting bad merge

--- vue/components/ui/molecules/lineup/lineup.vue
+++ vue/components/ui/molecules/lineup/lineup.vue
@@ -75,43 +75,52 @@
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
-
 .lineup {
     text-align: left;
 }
-
-.lineup .lineup-item {
+.lineup > .lineup-item {
     border-bottom: 1px solid $lighter-grey;
+    padding: 16px 20px 16px 20px;
 }
-
-.lineup .lineup-item.clickable {
+.lineup > .lineup-item.clickable {
     cursor: pointer;
 }
-
-.lineup .lineup-item:first-child {
+.lineup > .lineup-item:first-child {
     border-top: 1px solid $lighter-grey;
 }
-
-.lineup .lineup-item .lineup-item-value {
-    display: inline-block;
+.lineup > .lineup-item ::v-deep .lineup-item-value {
     box-sizing: border-box;
-    padding: 0px 15px 0px 15px;
-}
-
-.lineup .lineup-item .lineup-item-value .key {
+    display: inline-block;
+    font-weight: 600;
+    min-height: 65px;
+    overflow: hidden;
+    padding: 20px 20px 0px 0px;
+    text-align: left;
     text-transform: uppercase;
-    font-size: 12px;
-    font-weight: bold;
-    letter-spacing: 0.6px;
-    color: #8d8d8d;
 }
-
-.lineup .lineup-item .lineup-item-value .value {
-  font-size: 14px;
-  font-weight: bold;
-  letter-spacing: 0.7px;
-  color: #0d0d0d;
-  margin: 6px 0px 0px 0px;
+.lineup > .lineup-item ::v-deep .lineup-item-value .label > p,
+.lineup > .lineup-item ::v-deep .lineup-item-value .value > p,
+.lineup > .lineup-item ::v-deep .lineup-item-value .note > p {
+    margin: 0px 0px 0px 0px;
+}
+.lineup > .lineup-item .lineup-item-value .label {
+    color: $label-color;
+    font-size: 12px;
+    margin: 0px 0px 6px 0px;
+}
+.lineup > .lineup-item .lineup-item-value .value {
+    font-size: 14px;
+    line-height: 18px;
+    margin: 6px 0px 0px 0px;
+    word-break: break-all;
+    word-break: break-word;
+}
+.lineup > .lineup-item .lineup-item-value .note {
+    color: $label-color;
+    font-size: 11px;
+    line-height: 16px;
+    margin-bottom: 0px;
+    margin-top: 2px;
 }
 </style>
 
