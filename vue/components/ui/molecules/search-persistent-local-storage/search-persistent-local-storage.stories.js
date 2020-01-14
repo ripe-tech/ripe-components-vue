705c648e3068034edcfe2da5d714ffbc8185316f
Nuno Silva <nuno_nfss96@hotmail.com>
2020-01-14 15:56:42 +0000

    Adding props + pretty

--- vue/components/ui/molecules/search-persistent-local-storage/search-persistent-local-storage.stories.js
+++ vue/components/ui/molecules/search-persistent-local-storage/search-persistent-local-storage.stories.js
@@ -1,17 +1,87 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text, boolean, number, select } from "@storybook/addon-knobs";
 
 storiesOf("Molecules", module)
     .addDecorator(withKnobs)
     .add("Search Persistence with local storage", () => ({
         props: {
+            value: {
+                default: text("Value", "")
+            },
+            filters: {
+                type: Array,
+                default: () => [
+                    {
+                        name: "Filter 1",
+                        value: "brand=dummy",
+                        tenancy: "brand",
+                        context: "swear",
+                        metadata: { lan: "en" }
+                    },
+                    {
+                        name: "Filter 2 with a big ammount of characters in its name",
+                        value: "status=pending",
+                        tenancy: "factory",
+                        context: "barbara_guimaraes",
+                        metadata: { lan: "en" }
+                    },
+                    {
+                        name: "Filter 3",
+                        value: "status=created",
+                        tenancy: "factory",
+                        context: "barbara_guimaraes",
+                        metadata: { lan: "en" }
+                    },
+                    {
+                        name: "Filter 4",
+                        value: "status=sent",
+                        tenancy: "factory",
+                        context: "coimbroes_33",
+                        metadata: { lan: "en" }
+                    }
+                ]
+            },
+            placeholder: {
+                default: text("Placeholder", "This is a search input")
+            },
+            iconVisible: {
+                default: boolean("Icon Visible", true)
+            },
+            clearVisible: {
+                default: boolean("Clear Visible", true)
+            },
+            variant: {
+                default: select(
+                    "Variant",
+                    {
+                        Unset: null,
+                        Dark: "dark"
+                    },
+                    null
+                )
+            },
+            width: {
+                default: number("Width", null)
+            },
+            loading: {
+                default: boolean("Loading", false)
+            }
         },
         data: function() {
             return {};
         },
         template: `
             <div>
-                <search-persistent-local-storage />
+                <search-persistent-local-storage
+                    v-bind:value.sync="valueData"
+                    v-bind:filters.sync="filtersData"
+                    v-bind:placeholder="placeholder"
+                    v-bind:icon-visible="iconVisible"
+                    v-bind:clear-visible="clearVisible"
+                    v-bind:variant="variant"
+                    v-bind:width="width"
+                    v-bind:loading="loading"
+                    />
             </div>
         `
     }));
--- vue/components/ui/molecules/search-persistent-local-storage/search-persistent-local-storage.vue
+++ vue/components/ui/molecules/search-persistent-local-storage/search-persistent-local-storage.vue
@@ -1,23 +1,64 @@
 <template>
-    <h1>Hello from search-persistent-local-storage !</h1>
+    <div class="search-persistent-local-storage">
+        <global />
+        <search-persistent
+            v-bind:value.sync="valueData"
+            v-bind:filters.sync="filtersData"
+            v-bind:placeholder="placeholder"
+            v-bind:icon-visible="iconVisible"
+            v-bind:clear-visible="clearVisible"
+            v-bind:variant="variant"
+            v-bind:width="width"
+            v-bind:loading="loading"
+        />
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
-
 </style>
 
 <script>
 export const SearchPersistentLocalStorage = {
     name: "search-persistent-local-storage",
     props: {
+        variant: {
+            type: String,
+            default: null
+        },
+        value: {
+            type: String,
+            default: null
+        },
+        filters: {
+            type: Array,
+            default: () => []
+        },
+        placeholder: {
+            type: String,
+            default: "Search"
+        },
+        iconVisible: {
+            type: Boolean,
+            default: true
+        },
+        clearVisible: {
+            type: Boolean,
+            default: true
+        },
+        width: {
+            type: Number,
+            default: null
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        }
     },
     data: function() {
-        return {
-        };
+        return {};
     },
-    methods: {
-    }
+    methods: {}
 };
 
 export default SearchPersistentLocalStorage;
