1bbc3cc969191856f0c3b265a19c52adc5508d74
Flavio Lima <limfr.flavio.work@gmail.com>
2020-01-21 09:08:19 +0000

    Created component dynamic table

---
+++ vue/components/ui/molecules/dynamic-table/dynamic-table.stories.js
@@ -0,0 +1,90 @@
+import { storiesOf } from "@storybook/vue";
+import { withKnobs, select, boolean } from "@storybook/addon-knobs";
+
+storiesOf("Molecules", module)
+    .addDecorator(withKnobs)
+    .add("DynamicTable", () => ({
+        props: {
+            sort: {
+                default: select(
+                    "Sort Column",
+                    {
+                        Unset: null,
+                        Id: "id",
+                        User: "user",
+                        System: "system"
+                    },
+                    null
+                )
+            },
+            reverse: {
+                default: () => boolean("Reverse", null)
+            },
+            mockItems: {
+                default: () => [
+                    {
+                        id: 1,
+                        user: "Bill Gates",
+                        system: "Windows"
+                    },
+                    {
+                        id: 2,
+                        user: "Steve Jobs",
+                        system: "Macintosh"
+                    },
+                    {
+                        id: 3,
+                        user: "Linus Torvalds",
+                        system: "Linux"
+                    }
+                ]
+            },
+            mockColumns: {
+                type: Array,
+                default: () => [
+                    { value: "id", label: "ID" },
+                    { value: "user", label: "User" },
+                    { value: "system", label: "System" }
+                ]
+            }
+        },
+        data: function() {
+            return {
+                reverseData: this.reverse,
+                sortData: this.sort,
+                editItemClicked: null,
+                optionsItemClicked: null
+            };
+        },
+        watch: {
+            sort(value) {
+                this.sortData = value;
+            },
+            reverse(value) {
+                this.reverseData = value;
+            }
+        },
+        methods: {
+            editPressed(item) {
+                this.editItemClicked = item;
+            },
+            optionsPressed(item) {
+                this.optionsItemClicked = item;
+            }
+        },
+        template: `
+            <div>
+                <dynamic-table
+                    class="dynamic-table"
+                    v-bind:columns="mockColumns"
+                    v-bind:items="mockItems"
+                    v-bind:button-offset="200"
+                    v-on:click:edit="editPressed"
+                    v-on:click:item-options="optionsPressed"
+                    v-bind:sort.sync="sortData"
+                    v-bind:reverse.sync="reverseData"
+                />
+                <p>Edit clicked: {{ this.editItemClicked }},<br>  Item Options clicked: {{ this.optionsItemClicked }}</p>
+            </div>
+        `
+    }));
---
+++ vue/components/ui/molecules/dynamic-table/dynamic-table.vue
@@ -0,0 +1,166 @@
+<template>
+    <div class="dynamic-table" v-on:mouseleave="mouseLeave">
+        <table-ripe
+            v-bind:columns="columns"
+            v-bind:items="items"
+            v-bind:sort-method="sortMethod"
+            v-bind:transition="transition"
+            v-bind:sort="sort"
+            v-bind:header-height="headerHeight"
+            v-bind:reverse="reverse"
+            v-on:mouse:over="mouseOver"
+        />
+        <div class="column-container" v-bind:style="columnStyle">
+            <div
+                class="button-container"
+                v-bind:class="{ hovered: hoveredItem === index }"
+                v-for="(item, index) in items"
+                v-bind:key="index"
+            >
+                <button-color
+                    v-bind:text="'Edit'"
+                    v-bind:secondary="true"
+                    v-bind:icon="'edit'"
+                    v-bind:min-width="86"
+                    v-on:click="editClicked"
+                />
+                <button-color
+                    v-bind:text="''"
+                    v-bind:secondary="true"
+                    v-bind:icon="'chevron-down'"
+                    v-bind:min-width="48"
+                    v-on:click.stop="openDropdown"
+                />
+                <dropdown
+                    class="dropdown"
+                    v-bind:items="dropdownOptions"
+                    v-bind:visible.sync="dropdownVisible"
+                    v-bind:class="{ inactive: !dropdownVisible }"
+                    v-on:item-clicked="dropdownItemClicked"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "css/variables.scss";
+
+.dynamic-table {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+}
+
+body.mobile .dynamic-table {
+    display: none;
+}
+
+.dynamic-table .column-container {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    width: 0px;
+}
+
+.dynamic-table .column-container .button-container {
+    align-items: center;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+}
+
+.dynamic-table .column-container .button-container.hovered {
+    overflow: visible;
+}
+
+.dynamic-table .dropdown {
+    min-width: 150px;
+    position: absolute;
+}
+
+.dynamic-table .dropdown.inactive {
+    border: none;
+    box-shadow: none;
+}
+</style>
+
+<script>
+export const DynamicTable = {
+    name: "DynamicTable",
+    props: {
+        columns: {
+            type: Array,
+            default: () => []
+        },
+        items: {
+            type: Array,
+            default: () => []
+        },
+        sortMethod: {
+            type: Function
+        },
+        transition: {
+            type: String
+        },
+        sort: {
+            type: String
+        },
+        reverse: {
+            type: Boolean
+        },
+        headerHeight: {
+            type: Number,
+            default: 36
+        },
+        buttonOffset: {
+            type: Number,
+            default: 200
+        }
+    },
+    data: function() {
+        return {
+            hoveredItem: null,
+            dropdownVisible: false
+        };
+    },
+    computed: {
+        dropdownOptions() {
+            return [
+                { value: "duplicate", label: "Duplicate" },
+                { value: "delete", label: "Delete" }
+            ];
+        },
+        columnStyle() {
+            return {
+                margin: `${this.headerHeight}px 0px 0px 0px`,
+                height: `calc(100% - ${this.headerHeight}px`,
+                right: `${this.buttonOffset}px`
+            };
+        }
+    },
+    methods: {
+        mouseLeave() {
+            this.dropdownVisible = false;
+            this.hoveredItem = null;
+        },
+        mouseOver(item, index) {
+            this.dropdownVisible = false;
+            this.hoveredItem = index;
+        },
+        editClicked() {
+            this.$emit("click:edit", this.hoveredItem);
+        },
+        openDropdown() {
+            this.dropdownVisible = !this.dropdownVisible;
+        },
+        dropdownItemClicked(item) {
+            this.$emit("click:item-options", { value: item.value, index: this.hoveredItem });
+        }
+    }
+};
+
+export default DynamicTable;
+</script>
