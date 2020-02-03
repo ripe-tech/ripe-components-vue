c47b3d6f066ae72bd8c2983079fc4d6d208d3d65
joamag <joamag@hive.pt>
2020-02-03 19:17:40 +0000

    Image component replace

--- vue/components/ui/organisms/header/header.vue
+++ vue/components/ui/organisms/header/header.vue
@@ -14,7 +14,7 @@
                         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
                     </svg>
                     <router-link class="header-logo-container" to="/">
-                        <img class="header-logo" v-bind:src="logo" />
+                        <image-ripe class="header-logo" v-bind:src="logo" />
                     </router-link>
                     <search
                         v-bind:placeholder="searchPlaceholder"
