d6313d1a91932b0cb35c05e804679a1520c3c229
3rdvision <kazgru@gmail.com>
2020-02-11 11:57:11 +0000

    Add safe loading

--- vue/components/ui/atoms/icon/icon.vue
+++ vue/components/ui/atoms/icon/icon.vue
@@ -3,7 +3,7 @@
         class="icon"
         v-show="icon"
         v-on:click="onClick"
-        v-html="require(`!!raw-loader!./../../../../assets/icons/black/${icon}.svg`).default"
+        v-html="svgFile"
     />
 </template>
 
@@ -28,12 +28,23 @@ export const Icon = {
             this.setSvgAttributes(value);
         }
     },
+    computed: {
+        svgFile() {
+            try {
+                return require(`!!raw-loader!./../../../../assets/icons/black/${this.icon}.svg`).default;
+            }
+            catch (error) {
+                console.error(`Error loading icon '${this.icon}'.`, error);
+            }
+        }
+    },
     methods: {
         onClick(event) {
             this.$emit("click", event);
         },
         setSvgAttributes(attributes) {
             this.$nextTick(() => {
+                if (!this.$el.firstElementChild) return;
                 for (const [key, value] of Object.entries(attributes)) {
                     this.$el.firstElementChild.setAttribute(key, value);
                 }
