4548c5a3663fa5d620fa72259b038bb7b82c0f06
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-18 14:29:52 +0000

    Breadcrumbs style

--- vue/components/ui/atoms/breadcrumbs/breadcrumbs.vue
+++ vue/components/ui/atoms/breadcrumbs/breadcrumbs.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="breadcrumbs">
+    <div class="breadcrumbs" v-bind:style="style">
         <template v-for="(breadcrumb, index) in breadcrumbs">
             <template v-if="index != Object.keys(breadcrumbs).length - 1">
-                <link-ripe v-bind:key="index" v-bind:text="breadcrumb.text" v-bind:href="breadcrumb.route" v-bind:color="'grey'"/>
-                <div v-bind:key="`${index}-division`" class="breadcrumb-division">/</div>
+                <link-ripe v-bind:key="index" v-bind:text="breadcrumb.text" v-bind:href="breadcrumb.route" v-bind:hover="'color'" />
+<!--                 <div v-bind:key="`${index}-division`" class="breadcrumb-division">&nbsp&nbsp/&nbsp&nbsp&nbsp</div> -->
             </template>
             <div v-else v-bind:key="index" class="breadcrumb-last">{{breadcrumb.text}}</div>
         </template>
@@ -12,6 +12,20 @@
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
+
+.breadcrumbs > * {
+    display: inline-block;
+}
+
+.breadcrumbs > .link,
+.breadcrumbs > .breadcrumb-division {
+    color: #a2adb6;
+}
+
+.breadcrumbs .breadcrumb-last {
+    color: $light-black;
+}
+
 </style>
 
 <script>
@@ -21,6 +35,15 @@ export const Breadcrumbs = {
         breadcrumbs: {
             type: Array,
             default: () => []
+        },
+        fontSize: {
+            type: Number,
+            default: 26
+        }
+    },
+    computed: {
+        style() {
+            return { "font-size": `${this.fontSize}px`}
         }
     }
 };
