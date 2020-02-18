275ff209c2e853b87c74999c646c087b84eb54e6
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-18 16:47:15 +0000

    Rough elements logic and organization

--- vue/components/ui/molecules/progress-list-item/progress-list-item.vue
+++ vue/components/ui/molecules/progress-list-item/progress-list-item.vue
@@ -2,17 +2,69 @@
     <div class="progress-list-item">
         <div class="information-container">
             <div class="information">
-                <div class="title">{{ title }}</div>
-                <div class="description">{{ description }}</div>
+                <div class="title">
+                    {{ title }}
+                </div>
+                <div class="description">
+                    {{ description }}
+                </div>
             </div>
-            <div class="state">{{ actionText }}<br/>{{ actionIcon }}<br/>{{ finished }}</div>            
+            <div class="state">
+                <template v-if="finished">
+                    <button-icon
+                        v-bind:icon="actionIcon"
+                        v-bind:text="actionText"
+                        v-bind:size="24"
+                    />
+                    <icon
+                        v-bind:icon="'ok'"
+                        v-bind:color="'#45a777'"
+                        v-bind:stroke-width="2"
+                        v-bind:width="24"
+                        v-bind:height="24"
+                    />
+                </template>
+                <button-icon v-bind:icon="'close'" v-bind:size="24" v-else />
+            </div>
         </div>
-        <div class="progress">{{ progress }} %</div>
+        <div class="progress" v-if="progress !== null && !finished">
+            <progress-bar
+                v-bind:steps="100"
+                v-bind:current-step="progress"
+                v-bind:color="'#1d2631'"
+                v-bind:label="`${progress} %`"
+            />
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
+
+.progress-list-item .information-container .information,
+.progress-list-item .information-container .state {
+    display: inline-block;
+}
+
+.progress-list-item .information-container .information {
+    //background-color: lightblue;
+}
+
+.progress-list-item .information-container .information .title {
+    font-weight: bold;
+}
+
+.progress-list-item .information-container .information .description {
+    //color: gray;
+}
+
+.progress-list-item .information-container .state {
+    //background-color: lightcoral;
+}
+
+.progress-list-item .progress {
+    //background-color: lightgreen;
+}
 </style>
 
 <script>
