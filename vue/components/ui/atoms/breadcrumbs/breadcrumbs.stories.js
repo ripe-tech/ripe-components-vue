9743ec677113df204db78a8263b2b7bf1f04f7c1
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-18 12:31:24 +0000

    Added bradcrumbs prop

--- vue/components/ui/atoms/breadcrumbs/breadcrumbs.stories.js
+++ vue/components/ui/atoms/breadcrumbs/breadcrumbs.stories.js
@@ -4,10 +4,25 @@ import { withKnobs } from "@storybook/addon-knobs";
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Breadcrumbs", () => ({
-        props: {},
+        props: {
+            breadcrumbs: {
+                type: Array,
+                default: () => [
+                    {
+                        text: "Builds",
+                        route: "https://id.platforme.com/admin/accounts/ns%40platforme.com/avatar"
+                    },
+                    {
+                        text: "Swear",
+                        route: "https://id.platforme.com/admin/accounts/t-ms%40platforme.com/avatar"
+                    },
+                    { text: 7 }
+                ]
+            }
+        },
         template: `
             <div>
-                <breadcrumbs />
+                <breadcrumbs v-bind:breadcrumbs="breadcrumbs" />
             </div>
         `
     }));
--- vue/components/ui/atoms/breadcrumbs/breadcrumbs.vue
+++ vue/components/ui/atoms/breadcrumbs/breadcrumbs.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="breadcrumbs">
         <h1>Hi! I'm breadcrumbs</h1>
+        {{ breadcrumbs }}
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
-
 </style>
 
 <script>
 export const Breadcrumbs = {
     name: "breadcrumbs",
-    props: {}
+    props: {
+        breadcrumbs: {
+            type: Array,
+            default: () => []
+        }
+    }
 };
 
 export default Breadcrumbs;
