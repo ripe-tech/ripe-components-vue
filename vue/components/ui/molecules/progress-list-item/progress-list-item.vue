2a1a417bb91f53f43330f2a17771c47ac1daf029
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-19 08:43:02 +0000

    Container style

--- vue/components/ui/molecules/progress-list-item/progress-list-item.vue
+++ vue/components/ui/molecules/progress-list-item/progress-list-item.vue
@@ -43,6 +43,8 @@
 
 .progress-list-item {
     padding: 16px 20px 16px 20px;
+    border-radius: 6px;
+    box-shadow: 0 2px 5px 0 rgba(67, 86, 100, 0.25);
 }
 
 .progress-list-item .information-container .information,
