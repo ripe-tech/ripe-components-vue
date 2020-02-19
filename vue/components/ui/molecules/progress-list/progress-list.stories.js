695c5a87369cb6aaf5903e4ca5fb356358721a63
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-19 16:16:11 +0000

    Fixed storybook category

--- vue/components/ui/molecules/progress-list/progress-list.stories.js
+++ vue/components/ui/molecules/progress-list/progress-list.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, text } from "@storybook/addon-knobs";
 
-storiesOf("Atoms", module)
+storiesOf("Molecules", module)
     .addDecorator(withKnobs)
     .add("Progress List", () => ({
         props: {
