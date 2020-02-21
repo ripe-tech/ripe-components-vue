02974b9ae96399bf1aa84b98ad26552a97d77c56
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-21 12:33:49 +0000

    Animation visualizer component working

--- vue/components/ui/animations/animation-visualizer/animation-visualizer.stories.js
+++ vue/components/ui/animations/animation-visualizer/animation-visualizer.stories.js
@@ -1,12 +1,48 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
 storiesOf("Animations", module)
     .addDecorator(withKnobs)
-    .add("Animations Visualizer", () => ({
+    .add("Animation Visualizer", () => ({
+        props: {
+            title: {
+                default: text("Title", null)
+            },
+            animationName: {
+                default: text("Animation Name", null)
+            },
+            animationDuration: {
+                default: text("Animation Duration", null)
+            },
+            animationTimingFunction: {
+                default: text("Animation Timing Function", null)
+            },
+            animationDelay: {
+                default: text("Animation Delay", null)
+            },
+            animationIterationCount: {
+                default: text("Animation Iteration Count", null)
+            },
+            animationDirection: {
+                default: text("Animation Direction", null)
+            },
+            animationMode: {
+                default: text("Animation Mode", null)
+            }
+        },
         template: `
             <div>
-                <h1>Animations Visualizer</h1>
+                <animation-visualizer
+                    v-bind:title="title"
+                    v-bind:text="text"
+                    v-bind:animation-name="animationName"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timingFunction="animationTimingFunction"
+                    v-bind:animation-delay="animationDelay"
+                    v-bind:animation-iterationCount="animationIterationCount"
+                    v-bind:animation-direction="animationDirection"
+                    v-bind:animation-mode="animationMode"
+                />
             </div>
         `
     }));
--- vue/components/ui/animations/animation-visualizer/animation-visualizer.vue
+++ vue/components/ui/animations/animation-visualizer/animation-visualizer.vue
@@ -1,20 +1,75 @@
 <template>
     <div class="animation-visualizer">
-        <p>fawgwagwag</p>
+        <h3 v-if="title">{{ title }}</h3>
+        <div class="box" v-bind:style="animationStyle">
+            Box
+        </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
-@import "css/variables.scss";
+<style lang="scss">
+@import "css/animations.scss";
 
-
+.box {
+    background-color: #ff00ff;
+    font-weight: bold;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    width: 50px;
+}
 </style>
 
 <script>
 export const AnimationVisualizer = {
     name: "animation-visualizer",
     props: {
+        title: {
+            type: String,
+            default: null
+        },
+        animationName: {
+            type: String,
+            required: true
+        },
+        animationDuration: {
+            type: String,
+            required: true
+        },
+        animationTimingFunction: {
+            type: String,
+            default: null
+        },
+        animationDelay: {
+            type: String,
+            default: null
+        },
+        animationIterationCount: {
+            type: String,
+            default: "infinite"
+        },
+        animationDirection: {
+            type: String,
+            default: null
+        },
+        animationMode: {
+            type: String,
+            default: null
+        }
     },
+    computed: {
+        animationStyle() {
+            return {
+                "animation-name": this.animationName,
+                "animation-duration": this.animationDuration,
+                "animation-timing-function": this.animationTimingFunction,
+                "animation-delay": this.animationDelay,
+                "animation-iteration-count": this.animationIterationCount,
+                "animation-direction": this.animationDirection,
+                "animation-fill-mode": this.animationMode
+            };
+        }
+    }
 };
 
 export default AnimationVisualizer;
