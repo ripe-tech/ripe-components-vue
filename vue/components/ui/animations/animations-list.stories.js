597b64e2779caeec9851edc791ad1f19699dc266
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-21 15:42:51 +0000

    Fixed Animations List

--- vue/components/ui/animations/animations-list.stories.js
+++ vue/components/ui/animations/animations-list.stories.js
@@ -11,16 +11,69 @@ storiesOf("Animations", module)
         },
         template: `
             <div>
-                <animation-visualizer v-bind:title="'fade-in'" v-bind:animation-name="'fade-in'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'fade-into'" v-bind:animation-name="'fade-into'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'fade-into-drop'" v-bind:animation-name="'fade-into-drop'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'fade-into-rise'" v-bind:animation-name="'fade-into-rise'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'fade-grow'" v-bind:animation-name="'fade-grow'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'fade-grow-rise'" v-bind:animation-name="'fade-grow-rise'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'fade-shrink-visibility'" v-bind:animation-name="'fade-shrink-visibility'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'slide-left-fake'" v-bind:animation-name="'slide-left-fake'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'slide-right-fake'" v-bind:animation-name="'slide-right-fake'" v-bind:animation-duration="animationDuration" />
-                <animation-visualizer v-bind:title="'zoom'" v-bind:animation-name="'zoom'" v-bind:animation-duration="animationDuration" />
+                <animation-visualizer
+                    v-bind:title="'fade-in'"
+                    v-bind:animation-name="'fade-in'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'fade-into'"
+                    v-bind:animation-name="'fade-into'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'fade-into-drop'" 
+                    v-bind:animation-name="'fade-into-drop'" 
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'fade-into-rise'" 
+                    v-bind:animation-name="'fade-into-rise'" 
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'fade-grow'"
+                    v-bind:animation-name="'fade-grow'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'fade-grow-rise'" 
+                    v-bind:animation-name="'fade-grow-rise'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'fade-shrink-visibility'" 
+                    v-bind:animation-name="'fade-shrink-visibility'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'slide-left-fake'" 
+                    v-bind:animation-name="'slide-left-fake'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'slide-right-fake'"
+                    v-bind:animation-name="'slide-right-fake'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
+                <animation-visualizer 
+                    v-bind:title="'zoom'"
+                    v-bind:animation-name="'zoom'"
+                    v-bind:animation-duration="animationDuration"
+                    v-bind:animation-timing-function="'infinite'"
+                />
             </div>
         `
     }));
+
+/*     slide-left-fake
+    cubic-bezier(0.05, 0.95, 0.05, 0.95) */
