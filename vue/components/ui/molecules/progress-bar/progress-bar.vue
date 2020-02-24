40217c3fd2f7177cf4e7adc7b669a4328e286f57
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-24 10:09:41 +0000

    Configurable fill transition

--- vue/components/ui/molecules/progress-bar/progress-bar.vue
+++ vue/components/ui/molecules/progress-bar/progress-bar.vue
@@ -32,7 +32,6 @@
 .fill {
     background-color: #4071f2;
     height: 100%;
-    transition: width 0.5s ease;
     width: 1%;
 }
 </style>
@@ -60,6 +59,14 @@ export const ProgressBar = {
         labelAlignment: {
             type: String,
             default: null
+        },
+        fillTransitonTime: {
+            type: Number,
+            default: 0.5
+        },
+        fillTransitonMode: {
+            type: String,
+            default: "ease"
         }
     },
     computed: {
@@ -72,7 +79,8 @@ export const ProgressBar = {
         fillStyle() {
             return {
                 "background-color": this.color || "#4071f2",
-                width: `${(this.currentStep / this.steps) * 100}%`
+                width: `${(this.currentStep / this.steps) * 100}%`,
+                transition: `width ${this.fillTransitonTime}s ${this.fillTransitonMode}`
             };
         }
     }
