bf637660a78a2790a8ab4c4314ad18a6c9dfee6e
Nuno Silva <nuno_nfss96@hotmail.com>
2020-03-03 12:05:02 +0000

    In sync with progress-list branch

--- vue/components/ui/molecules/progress-list/progress-list.vue
+++ vue/components/ui/molecules/progress-list/progress-list.vue
@@ -1,16 +1,46 @@
 <template>
     <div class="progress-list">
-        <template v-for="(item, index) in items">
-            <div v-bind:key="index">
-                {{ index }}-{{ item }}
+        <div class="progres-list-items">
+            <progress-list-item
+                v-bind:title="item.title"
+                v-bind:description="item.description"
+                v-bind:finished="item.finished"
+                v-bind:progress="item.progress"
+                v-bind:action-text="item.actionText"
+                v-bind:action-icon="item.actionIcon"
+                v-for="(item, index) in items"
+                v-bind:key="index"
+            />
+        </div>
+        <slot>
+            <div class="footer">
+                {{ footerText }}
             </div>
-        </template>
-        {{ footerText }}
+        </slot>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
+
+.progress-list {
+    background-color: $soft-blue;
+    padding: 18px 24px 18px 24px;
+}
+
+.progress-list > .progres-list-items {
+    margin: 22px 0px 30px 0px;
+}
+
+.progress-list > .progres-list-items > .progress-list-item {
+    margin: 0px 0px 18px 0px;
+}
+
+.progress-list > .footer {
+    color: #57626e;
+    font-size: 14px;
+    letter-spacing: 0.3px;
+}
 </style>
 
 <script>
