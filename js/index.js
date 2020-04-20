613a235fcdb60af6ebd5ac161285ce1d4bcc5e6e
joamag <joamag@hive.pt>
2020-04-20 15:19:20 +0100

    Added serializeContext and added new webpack lib

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 export { getApps } from "./apps";
 export { Assets } from "./assets";
+export { serializeContext } from "./context";
 export { filterToParams } from "./filter";
 export { normalize } from "./util";
