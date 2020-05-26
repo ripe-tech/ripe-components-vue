8df7e073a369f66f27b6b2cdfc032f2745c95434
joamag <joamag@gmail.com>
2020-05-26 18:48:33 +0100

    Initial config support in component vue

---
+++ vue/mixins/config.js
@@ -0,0 +1,23 @@
+import { hasFeature, setFeature, setFeatures, bindFeature, unbindFeature } from "../../js";
+
+const configMixin = {
+    methods: {
+        hasFeature(name) {
+            return hasFeature(name);
+        },
+        setFeature(name, value) {
+            return setFeature(name, value);
+        },
+        setFeatures(features) {
+            return setFeatures(features);
+        },
+        bindFeature(callable) {
+            return bindFeature(callable);
+        },
+        unbindFeature(callable) {
+            return unbindFeature(callable);
+        }
+    }
+};
+
+export { configMixin };
--- vue/mixins/index.js
+++ vue/mixins/index.js
@@ -1,3 +1,4 @@
+export * from "./config";
 export * from "./device";
 export * from "./oauth";
 export * from "./part";
