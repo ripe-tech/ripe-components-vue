1979e8016535463177ec69aa055a46564607b40e
Gabriel Candal <gcc@platforme.com>
2020-05-27 18:29:58 +0100

    Remove console log

--- vue/components/ui/atoms/button-color/button-color.test.js
+++ vue/components/ui/atoms/button-color/button-color.test.js
@@ -1,8 +1,6 @@
 const assert = require("assert");
 const base = require("../../../../test/base");
 
-console.log("este");
-
 describe("ButtonColor", () => {
     it("Can use custom text", () => {
         const component = base.getComponent("ButtonColor", {
