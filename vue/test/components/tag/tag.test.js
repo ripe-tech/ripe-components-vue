630c9b4433e15b293771cd2800d84b9a137aae37
João Magalhães <joamag@gmail.com>
2020-06-18 18:48:40 +0100

    New casing

--- vue/test/components/tag/tag.test.js
+++ vue/test/components/tag/tag.test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const base = require("../../base");
 
 describe("Tag", () => {
-    it("Has text", () => {
+    it("has a valid text", () => {
         const text = "This is text";
         const component = base.getComponent("Tag", {
             props: { text: text }
