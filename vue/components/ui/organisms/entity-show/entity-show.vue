1e12de9158db4fbb0f3851c98b3e5d1834c9f7fa
Nuno Silva <nuno_nfss96@hotmail.com>
2020-09-29 15:58:14 +0100

    Created entity-show with minimum structure

---
+++ vue/components/ui/organisms/entity-show/entity-show.vue
@@ -0,0 +1,51 @@
+<template>
+    <details-ripe
+        v-bind:title="title"
+        v-bind:name="name"
+        v-bind:values="values"
+        v-bind:item="entity"
+        v-bind:get-items="getEntities"
+    >
+        <slot v-bind:name="slot" v-for="slot in Object.keys($slots)" v-bind:slot="slot" />
+        <template v-for="slot in Object.keys($scopedSlots)" v-bind:slot="slot" slot-scope="scope">
+            <slot v-bind:name="slot" v-bind="scope" />
+        </template>
+    </details-ripe>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script>
+export const EntityShow = {
+    name: "entity-show",
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        values: {
+            type: Array,
+            required: true
+        },
+        getEntity: {
+            type: Function,
+            required: true
+        },
+        getEntities: {
+            type: Function,
+            required: true
+        }
+    },
+    data: function() {
+        return {
+            entity: {}
+        };
+    }
+};
+
+export default EntityShow;
+</script>
--- vue/components/ui/organisms/index.js
+++ vue/components/ui/organisms/index.js
@@ -1,5 +1,6 @@
 import { Chat } from "./chat/chat.vue";
 import { Details } from "./details/details.vue";
+import { EntityShow } from "./entity-show/entity-show.vue";
 import { Header } from "./header/header.vue";
 import { Listing } from "./listing/listing.vue";
 import { TableMenu } from "./table-menu/table-menu.vue";
@@ -7,11 +8,12 @@ import { TableMenu } from "./table-menu/table-menu.vue";
 const install = Vue => {
     Vue.component("chat", Chat);
     Vue.component("details-ripe", Details);
+    Vue.component("entity-show", EntityShow);
     Vue.component("header-ripe", Header);
     Vue.component("listing", Listing);
     Vue.component("table-menu", TableMenu);
 };
 
-export { Chat, Details, Header, Listing, TableMenu };
+export { Chat, Details, EntityShow, Header, Listing, TableMenu };
 
 export default install;
