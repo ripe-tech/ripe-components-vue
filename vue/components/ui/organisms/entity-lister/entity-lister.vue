7164f46a567bfa3695a33090411ce2fd0a8e3f86
Nuno Silva <nuno_nfss96@hotmail.com>
2020-10-02 17:14:12 +0100

    Added options to edit and delete

--- vue/components/ui/organisms/entity-lister/entity-lister.vue
+++ vue/components/ui/organisms/entity-lister/entity-lister.vue
@@ -4,15 +4,31 @@
         v-bind:name="name"
         v-bind:title-text="_title"
         v-bind:create-url="createUrl"
-        v-bind:table-columns="columns"
+        v-bind:table-columns="_columns"
         v-bind:table-alignment="'left'"
-        v-bind:lineup-fields="columns"
+        v-bind:lineup-fields="_columns"
         v-bind:get-items="getEntities"
         v-on:click:table="onTableRowClick"
     >
         <slot v-bind:name="slot" v-for="slot in Object.keys($slots)" v-bind:slot="slot" />
         <template v-for="slot in Object.keys($scopedSlots)" v-bind:slot="slot" slot-scope="scope">
             <slot v-bind:name="slot" v-bind="scope" />
+        </template>
+        <template v-slot:table-cell-actions="{ item }">
+            <router-link
+                class="button-edit"
+                v-bind:to="_editRoute(item)"
+                v-if="editButton"
+            >
+                <button-icon v-bind:icon="'edit'" v-bind:text="'Edit'" v-bind:size="42" />
+            </router-link>
+            <button-icon
+                v-bind:icon="'bin'"
+                v-bind:text="'Delete'"
+                v-bind:size="42"
+                v-if="deleteEntity"
+                v-on:click="onDeleteButtonClick"
+            />
         </template>
     </listing>
 </template>
@@ -39,6 +55,10 @@ export const EntityLister = {
             type: String,
             default: null
         },
+        getShowRoute: {
+            type: Function,
+            default: null
+        },
         createButton: {
             type: Boolean,
             default: true
@@ -47,9 +67,21 @@ export const EntityLister = {
             type: String | Object,
             default: null
         },
-        getShowRoute: {
+        editButton: {
+            type: Boolean,
+            default: true
+        },
+        editRoute: {
+            type: String | Object,
+            default: null
+        },
+        deleteEntity: {
             type: Function,
             default: null
+        },
+        getEntityName: {
+            type: Function,
+            default: entity => entity.name
         }
     },
     computed: {
@@ -60,9 +92,25 @@ export const EntityLister = {
             if (!this.createButton) return null;
 
             return this.createRoute || { name: `${this.name}-create` };
+        },
+        actionsWidth() {
+            let width = 40;
+            if (this.editButton) width += 85;
+            if (this.deleteEntity) width += 100;
+            return width;
+        },
+        _columns() {
+            return this.editButton || this.deleteEntity
+                ? this.columns.concat([
+                      { value: "actions", label: "Actions", width: `${this.actionsWidth}px` }
+                  ])
+                : this.columns;
         }
     },
     methods: {
+        _editRoute(item) {
+            return this.editRoute || { name: `${this.name}-edit`, params: { id: item.id } };
+        },
         showRoute(item) {
             return this.getShowRoute
                 ? this.getShowRoute(item)
@@ -70,6 +118,15 @@ export const EntityLister = {
         },
         onTableRowClick(item) {
             this.$router.push(this.showRoute(item));
+        },
+        async onDeleteButtonClick(item) {
+            const confirmed = await this.alert(
+                `Are you sure you really want to <strong>delete ${this.name} ${this.getEntityName(
+                    item
+                )}</strong>?<br/>Please bare in mind that this action <strong>is not reversible</strong>!`,
+                { title: `Delete ${this.name} ${this.getEntityName(item)}` }
+            );
+            if (confirmed) await this.deleteEntity(item);
         }
     }
 };
