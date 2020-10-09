607be59cac496455315a1e134c8b8e1168c74f72
Nuno Silva <nuno_nfss96@hotmail.com>
2020-10-09 09:14:51 +0100

    Dot

--- vue/components/ui/organisms/entity-creator/entity-creator.vue
+++ vue/components/ui/organisms/entity-creator/entity-creator.vue
@@ -94,7 +94,7 @@ export const EntityCreator = {
          *             }
          *         ]
          *     ]
-         * }
+         * }.
          */
         fields: {
             type: Object,
