4640f46a2807656695ed95b7548f65f2d03a07d1
Margarida Silva <ms@platforme.com>
2020-10-09 09:35:33 +0100

    Fix storybook deprecated configs
    See https://github.com/ripe-tech/ripe-white/pull/604

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import GlobalEvents from "vue-global-events";
-import { configure } from "@storybook/vue";
 
 import { install as RipeComponentsVue } from "../vue";
 
@@ -10,10 +9,3 @@ Vue.use(RipeComponentsVue);
 Vue.component("global-events", GlobalEvents);
 
 Vue.prototype.$bus = new Vue();
-
-const req = require.context("../vue", true, /\.stories\.js$/);
-function loadStories() {
-    req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
