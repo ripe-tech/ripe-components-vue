9e64b0fe1518ef11bed49688b109d61919ad66b7
3rdvision <kazgru@gmail.com>
2020-10-26 16:12:13 +0000

    Add more tests to input-color

--- vue/test/components/input-color/input-color.test.js
+++ vue/test/components/input-color/input-color.test.js
@@ -3,8 +3,28 @@ const base = require("../../base");
 
 describe("InputColor", () => {
     it("should initialize the input color", () => {
-        const component = base.getComponent("InputColor");
+        const component = base.getComponent("InputColor", {
+            props: { value: "#ff0000" }
+        });
 
         assert.strictEqual(component.exists(), true);
+    });
+    it("should display the correct color sent as a prop", () => {
+        const component = base.getComponent("InputColor", {
+            props: { value: "#ff0000" }
+        });
+
+        const colorPicker = component.find(".color-picker");
+        assert.strictEqual(colorPicker.attributes("style").includes("rgb(255, 0, 0)"), true);
+    });
+    it("should set the correct color sent as a prop in the native input", () => {
+        const component = base.getComponent("InputColor", {
+            props: { value: "#ff0000" }
+        });
+
+        assert.strictEqual(component.vm.$data.valueData, "#ff0000");
+
+        const inputNative = component.find(".input-color-native");
+        assert.strictEqual(inputNative.element._value, "#ff0000");
     });
 });
