0469fe606636a3893ceed27f7a366233da07438d
NFSS10 <nuno_nfss96@hotmail.com>
2020-11-27 17:23:07 +0000

    Added more tests + Pretty

--- vue/test/components/select-checkboxes/select-checkboxes.test.js
+++ vue/test/components/select-checkboxes/select-checkboxes.test.js
@@ -34,20 +34,128 @@ describe("Section", () => {
         const checkboxes = dropdown.findAll(".checkboxes > .checkbox-group > .checkbox-item");
         assert.strictEqual(checkboxes.length, 7);
         assert.strictEqual(checkboxes.at(0).find(".checkbox.checked").exists(), false);
-        assert.strictEqual(checkboxes.at(0).find(".checkbox > .checkbox-input > .label").text(), "Japan");
+        assert.strictEqual(
+            checkboxes.at(0).find(".checkbox > .checkbox-input > .label").text(),
+            "Japan"
+        );
         assert.strictEqual(checkboxes.at(1).find(".checkbox.checked").exists(), true);
-        assert.strictEqual(checkboxes.at(1).find(".checkbox > .checkbox-input > .label").text(), "Morocco");
+        assert.strictEqual(
+            checkboxes.at(1).find(".checkbox > .checkbox-input > .label").text(),
+            "Morocco"
+        );
         assert.strictEqual(checkboxes.at(2).find(".checkbox.checked").exists(), false);
-        assert.strictEqual(checkboxes.at(2).find(".checkbox > .checkbox-input > .label").text(), "canada");
+        assert.strictEqual(
+            checkboxes.at(2).find(".checkbox > .checkbox-input > .label").text(),
+            "canada"
+        );
         assert.strictEqual(checkboxes.at(3).find(".checkbox.checked").exists(), false);
-        assert.strictEqual(checkboxes.at(3).find(".checkbox > .checkbox-input > .label").text(), "China");
+        assert.strictEqual(
+            checkboxes.at(3).find(".checkbox > .checkbox-input > .label").text(),
+            "China"
+        );
         assert.strictEqual(checkboxes.at(4).find(".checkbox.checked").exists(), false);
-        assert.strictEqual(checkboxes.at(4).find(".checkbox > .checkbox-input > .label").text(), "Dubai");
+        assert.strictEqual(
+            checkboxes.at(4).find(".checkbox > .checkbox-input > .label").text(),
+            "Dubai"
+        );
         assert.strictEqual(checkboxes.at(5).find(".checkbox.checked").exists(), true);
         assert.strictEqual(checkboxes.at(5).find(".checkbox.disabled").exists(), true);
-        assert.strictEqual(checkboxes.at(5).find(".checkbox > .checkbox-input > .label").text(), "Bali");
+        assert.strictEqual(
+            checkboxes.at(5).find(".checkbox > .checkbox-input > .label").text(),
+            "Bali"
+        );
         assert.strictEqual(checkboxes.at(6).find(".checkbox.checked").exists(), false);
         assert.strictEqual(checkboxes.at(6).find(".checkbox.error").exists(), true);
-        assert.strictEqual(checkboxes.at(6).find(".checkbox > .checkbox-input > .label").text(), "Tibet");
+        assert.strictEqual(
+            checkboxes.at(6).find(".checkbox > .checkbox-input > .label").text(),
+            "Tibet"
+        );
+    });
+
+    it("should change label via prop", async () => {
+        const component = base.getComponent("SelectCheckboxes", {
+            props: { label: "Label Example" }
+        });
+
+        assert.strictEqual(
+            component.find(".select > .select-container > .select-button").text(),
+            "Label Example"
+        );
+
+        await component.setProps({ label: "Updated Label" });
+        assert.strictEqual(
+            component.find(".select > .select-container > .select-button").text(),
+            "Updated Label"
+        );
+    });
+
+    it("should change checked value in allowed checkboxes", () => {
+        const component = base.getComponent("SelectCheckboxes", {
+            props: {
+                label: "Label Example",
+                items: [
+                    { label: "Japan", value: "japan" },
+                    { label: "Morocco", value: "morocco" },
+                    { value: "canada" },
+                    { label: "China", value: "china" },
+                    { label: "Dubai", value: "dubai" },
+                    { label: "Bali", value: "bali", disabled: true },
+                    { label: "Tibet", value: "tibet", error: true }
+                ],
+                values: {
+                    japan: false,
+                    morocco: true,
+                    canada: false,
+                    china: false,
+                    dubai: false,
+                    bali: true
+                }
+            }
+        });
+
+        assert.strictEqual(
+            component.find(".select > .select-container > .select-button").text(),
+            "Label Example"
+        );
+        const dropdown = component.find(".select > .select-container > .dropdown-container");
+        const checkboxes = dropdown.findAll(".checkboxes > .checkbox-group > .checkbox-item");
+        assert.strictEqual(checkboxes.length, 7);
+        assert.strictEqual(checkboxes.at(0).find(".checkbox.checked").exists(), false);
+        assert.strictEqual(
+            checkboxes.at(0).find(".checkbox > .checkbox-input > .label").text(),
+            "Japan"
+        );
+        assert.strictEqual(checkboxes.at(1).find(".checkbox.checked").exists(), true);
+        assert.strictEqual(
+            checkboxes.at(1).find(".checkbox > .checkbox-input > .label").text(),
+            "Morocco"
+        );
+        assert.strictEqual(checkboxes.at(2).find(".checkbox.checked").exists(), false);
+        assert.strictEqual(
+            checkboxes.at(2).find(".checkbox > .checkbox-input > .label").text(),
+            "canada"
+        );
+        assert.strictEqual(checkboxes.at(3).find(".checkbox.checked").exists(), false);
+        assert.strictEqual(
+            checkboxes.at(3).find(".checkbox > .checkbox-input > .label").text(),
+            "China"
+        );
+        assert.strictEqual(checkboxes.at(4).find(".checkbox.checked").exists(), false);
+        assert.strictEqual(
+            checkboxes.at(4).find(".checkbox > .checkbox-input > .label").text(),
+            "Dubai"
+        );
+        assert.strictEqual(checkboxes.at(5).find(".checkbox.checked").exists(), true);
+        assert.strictEqual(checkboxes.at(5).find(".checkbox.disabled").exists(), true);
+        assert.strictEqual(
+            checkboxes.at(5).find(".checkbox > .checkbox-input > .label").text(),
+            "Bali"
+        );
+        assert.strictEqual(checkboxes.at(6).find(".checkbox.checked").exists(), false);
+        assert.strictEqual(checkboxes.at(6).find(".checkbox.error").exists(), true);
+        assert.strictEqual(
+            checkboxes.at(6).find(".checkbox > .checkbox-input > .label").text(),
+            "Tibet"
+        );
     });
 });
