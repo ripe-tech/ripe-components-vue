b838326c72dd85e2fbb4862a5732833476910348
NFSS10 <nuno_nfss96@hotmail.com>
2020-12-15 15:59:48 +0000

    Storybook story with positon props

--- vue/components/ui/atoms/box-resizable/box-resizable.stories.js
+++ vue/components/ui/atoms/box-resizable/box-resizable.stories.js
@@ -1,12 +1,29 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, number } from "@storybook/addon-knobs";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Box Resizable", () => ({
-        props: {},
+        props: {
+            x0: {
+                default: number("X0", 50)
+            },
+            y0: {
+                default: number("Y0", 50)
+            },
+            x1: {
+                default: number("X1", 250)
+            },
+            y1: {
+                default: number("Y1", 150)
+            }
+        },
         template: `
             <box-resizable
+                v-bind:x0="x0"
+                v-bind:y0="y0"
+                v-bind:x1="x1"
+                v-bind:y1="y1"
             />
         `
     }));
