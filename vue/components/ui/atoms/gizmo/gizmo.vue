79426c04b18601af074e4363b3989788916ba5c4
NFSS10 <nuno_nfss96@hotmail.com>
2020-12-16 11:56:16 +0000

    Hardcoded gizmo UI

--- vue/components/ui/atoms/gizmo/gizmo.vue
+++ vue/components/ui/atoms/gizmo/gizmo.vue
@@ -1,9 +1,31 @@
 <template>
-    <div class="gizmo" />
+    <div class="gizmo">
+        <div class="gizmo-inner" />
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @import "css/variables.scss";
+
+.gizmo {
+    box-sizing: border-box;
+    padding: 10px 10px 10px 10px;
+    position: absolute;
+    transform: translate(-15px, -15px);
+}
+
+.gizmo > .gizmo-inner {
+    border-color: #ff0000;
+    border-style: solid;
+    border-width: 2px;
+    box-sizing: border-box;
+    height: 10px;
+    width: 10px;
+}
+
+.gizmo:hover > .gizmo-inner {
+    border-color: #0000ff;
+}
 </style>
 
 <script>
