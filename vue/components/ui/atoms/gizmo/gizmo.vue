a9607b0a8083067faf4bbfc99a33b507189e58f8
NFSS10 <nuno_nfss96@hotmail.com>
2020-12-16 11:39:17 +0000

    Added props

--- vue/components/ui/atoms/gizmo/gizmo.vue
+++ vue/components/ui/atoms/gizmo/gizmo.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="gizmo">
-        Hello from gizmo
-    </div>
+    <div class="gizmo" />
 </template>
 
 <style lang="scss" scoped>
@@ -11,7 +9,28 @@
 <script>
 export const Gizmo = {
     name: "gizmo",
-    props: {}
+    props: {
+        x: {
+            type: Number,
+            default: 0
+        },
+        y: {
+            type: Number,
+            default: 0
+        },
+        width: {
+            type: Number,
+            default: 10
+        },
+        height: {
+            type: Number,
+            default: 10
+        },
+        round: {
+            type: Boolean,
+            default: false
+        }
+    }
 };
 
 export default Gizmo;
