25f91acd82eed861ab41f6f9c89404f1c13e4812
3rdvision <kazgru@gmail.com>
2020-12-17 15:13:17 +0000

    Improve test and add test case

--- vue/test/components/steps/steps.test.js
+++ vue/test/components/steps/steps.test.js
@@ -8,13 +8,56 @@ describe("Steps", () => {
         });
         assert.strictEqual(component.exists(), true);
     });
-    it("should only highlight the completed steps", () => {
+    it("should only highlight the current step", () => {
         const component = base.getComponent("Steps", {
-            props: { steps: ["A", "B", "C"], currentStep: 1 }
+            props: { steps: ["A", "B", "C", "D", "E"], currentStep: 2 }
         });
         const steps = component.findAll(".step .highlight");
-        assert.strictEqual(steps.at(0).isVisible(), true);
-        assert.strictEqual(steps.at(1).isVisible(), false);
+        assert.strictEqual(steps.at(0).isVisible(), false);
+        assert.strictEqual(steps.at(1).isVisible(), true);
         assert.strictEqual(steps.at(2).isVisible(), false);
+        assert.strictEqual(steps.at(2).isVisible(), false);
+        assert.strictEqual(steps.at(2).isVisible(), false);
+    });
+    it("should have white background if step is not completed and gray if otherwise", () => {
+        const component = base.getComponent("Steps", {
+            props: { steps: ["A", "B", "C", "D", "E"], currentStep: 3 }
+        });
+        const steps = component.findAll(".step .step-number");
+        assert.strictEqual(
+            steps
+                .at(0)
+                .attributes("style")
+                .includes("rgb(89, 98, 109)"),
+            true
+        );
+        assert.strictEqual(
+            steps
+                .at(1)
+                .attributes("style")
+                .includes("rgb(89, 98, 109)"),
+            true
+        );
+        assert.strictEqual(
+            steps
+                .at(2)
+                .attributes("style")
+                .includes("rgb(89, 98, 109)"),
+            true
+        );
+        assert.strictEqual(
+            steps
+                .at(3)
+                .attributes("style")
+                .includes("rgb(255, 255, 255)"),
+            true
+        );
+        assert.strictEqual(
+            steps
+                .at(4)
+                .attributes("style")
+                .includes("rgb(255, 255, 255)"),
+            true
+        );
     });
 });
