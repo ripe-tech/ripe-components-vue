31e251537f1d42bc702200ac6f1a2a2b1d8f8ef8
NFSS10 <nuno_nfss96@hotmail.com>
2021-01-06 11:53:20 +0000

    Selectable buttons

--- vue/components/ui/atoms/scrollable-buttons/scrollable-buttons.vue
+++ vue/components/ui/atoms/scrollable-buttons/scrollable-buttons.vue
@@ -8,6 +8,7 @@
             v-bind:class="buttonClasses(item)"
             v-for="item in items"
             v-bind:key="item.value"
+            v-on:click="onButtonClick($event, item)"
         >
             <slot v-bind:item="item">
                 <slot v-bind:name="item.value" v-bind:item="item">
@@ -27,9 +28,25 @@
 .scrollable-buttons > .previous,
 .scrollable-buttons > .button,
 .scrollable-buttons > .next {
-     display: inline-block;
+    cursor: pointer;
+    display: inline-block;
 }
 
+.scrollable-buttons > .button {
+    font-size: 16px;
+    font-weight: 700;
+    opacity: 0.5;
+    transition: opacity 0.2s ease-out;
+}
+
+.scrollable-buttons > .button:hover,
+.scrollable-buttons > .button.hover {
+    opacity: 0.7;
+}
+
+.scrollable-buttons > .button.selected {
+    opacity: 1;
+}
 </style>
 
 <script>
@@ -39,12 +56,34 @@ export const ScrollableButtons = {
         items: {
             type: Array,
             default: () => []
+        },
+        selected: {
+            type: String,
+            default: null
+        }
+    },
+    data: function() {
+        return {
+            selectedData: this.selected
+        };
+    },
+    watch: {
+        selected(value) {
+            this.selectedData = value;
+        },
+        selectedData(value) {
+            this.$emit("update:selected", value);
         }
     },
     methods: {
+        onButtonClick(event, item) {
+            this.selectedData = item.value;
+            this.$emit("button-click", event, item);
+        },
         buttonClasses(item) {
             const base = {};
             base[`button-${item.value}`] = true;
+            if (item.value === this.selectedData) base.selected = true;
             return base;
         }
     }
