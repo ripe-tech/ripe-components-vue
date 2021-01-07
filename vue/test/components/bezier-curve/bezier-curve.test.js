cf27f3c439c574c788d4458eba378d669cc7cf8c
3rdvision <kazgru@gmail.com>
2021-01-07 11:36:15 +0000

    Adapt tests

--- vue/test/components/bezier-curve/bezier-curve.test.js
+++ vue/test/components/bezier-curve/bezier-curve.test.js
@@ -30,7 +30,8 @@ describe("BezierCurve", () => {
                 cx2: 203,
                 cy1: 641,
                 cy2: 431,
-                resolution: "640x480"
+                resolutionWidth: 640,
+                resolutionHeight: 480
             }
         });
 
@@ -52,13 +53,13 @@ describe("BezierCurve", () => {
             }
         });
 
-        assert.strictEqual(component.find("#slope-1").isVisible(), false);
-        assert.strictEqual(component.find("#slope-2").isVisible(), false);
-        assert.strictEqual(component.find("#endpoint-1").isVisible(), false);
-        assert.strictEqual(component.find("#endpoint-2").isVisible(), false);
-        assert.strictEqual(component.find("#controlpoint-1").isVisible(), false);
-        assert.strictEqual(component.find("#controlpoint-2").isVisible(), false);
-        assert.strictEqual(component.find("#curve").isVisible(), true);
+        assert.strictEqual(component.find(".slope-1").isVisible(), false);
+        assert.strictEqual(component.find(".slope-2").isVisible(), false);
+        assert.strictEqual(component.find(".end-point-1").isVisible(), false);
+        assert.strictEqual(component.find(".end-point-2").isVisible(), false);
+        assert.strictEqual(component.find(".control-point-1").isVisible(), false);
+        assert.strictEqual(component.find(".control-point-2").isVisible(), false);
+        assert.strictEqual(component.find(".curve").isVisible(), true);
     });
 
     it("should draw the correct curve based on sent props", () => {
@@ -77,34 +78,5 @@ describe("BezierCurve", () => {
 
         const curve = component.find(".curve");
         assert.strictEqual(curve.attributes("d"), "M 521, 157 C 777, 641, 203, 431, 593, 85");
-    });
-
-    it("should emit updated coordinates when a point clicked", async () => {
-        const component = base.getComponent("BezierCurve", {
-            props: {
-                x1: 127,
-                y1: 430,
-                x2: 557,
-                y2: 178,
-                cx1: 186,
-                cx2: 203,
-                cy1: 641,
-                cy2: 431
-            }
-        });
-
-        const controlPoint1 = component.find("#controlpoint-1");
-        controlPoint1.trigger("mousedown");
-        controlPoint1.trigger("mouseup");
-        await component.vm.$nextTick();
-        assert.strictEqual(component.emitted("update:cx1")[0][0], 186);
-        assert.strictEqual(component.emitted("update:cy1")[0][0], 641);
-
-        const controlPoint2 = component.find("#controlpoint-2");
-        controlPoint2.trigger("touchstart");
-        controlPoint2.trigger("touchend");
-        await component.vm.$nextTick();
-        assert.strictEqual(component.emitted("update:cx2")[0][0], 203);
-        assert.strictEqual(component.emitted("update:cy2")[0][0], 431);
     });
 });
