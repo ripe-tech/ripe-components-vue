38595288ee26968ad7622a2cdbdf7a94564f53a4
João Magalhães <joamag@gmail.com>
2021-01-12 11:56:57 +0000

    fix: add new doc strings

--- vue/mixins/download.js
+++ vue/mixins/download.js
@@ -1,14 +1,17 @@
 export const downloadMixin = {
     methods: {
         /**
-         * Downloads a file with a given filename.
+         * Downloads a file with a given filename using the provided
+         * Blob object as the basis for the creation of it.
          *
-         * @param {Object} obj Object to be downloaded.
-         * @param {String} filename Name to give the file.
+         * @param {Object} blob The Blob object that is going to be used
+         * as the basis for the URL generation in the download.
+         * @param {String} filename The name to be give to the file that
+         * is going to be downloaded.
          */
-        download(obj, filename) {
+        download(blob, filename) {
             const anchor = document.createElement("a");
-            const url = URL.createObjectURL(obj);
+            const url = URL.createObjectURL(blob);
             anchor.href = url;
             anchor.download = filename;
             document.body.appendChild(anchor);
