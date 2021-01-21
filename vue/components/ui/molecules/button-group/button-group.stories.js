41efb73c659e5e63f0a3df986c8a64715f1bd218
BeeMargarida <ms@platforme.com>
2021-01-21 16:40:56 +0000

    feat: improve button-group story

--- vue/components/ui/molecules/button-group/button-group.stories.js
+++ vue/components/ui/molecules/button-group/button-group.stories.js
@@ -8,10 +8,10 @@ storiesOf("Molecules", module)
             items: {
                 type: Array,
                 default: () => [
-                    { value: "1", label: "Item 1" },
+                    { value: "1", label: "Item 1", color: "blue" },
                     { value: "2", label: "Item 2" },
                     { value: "3", label: "Item 3" },
-                    { value: "4", label: "Item 4" },
+                    { value: "4", label: "Item 4", color: "red" },
                     { value: "5", label: "Item 5" }
                 ]
             },
