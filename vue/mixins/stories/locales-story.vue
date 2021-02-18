8858efa917a07ec7bf1a500c28c1be557afffb55
NFSS10 <nuno_nfss96@hotmail.com>
2021-02-18 18:00:27 +0000

    feat: workaround storybook mixins not working properly

---
+++ vue/mixins/stories/locales-story.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="locales-story">
+        <div class="info">
+            <div><b>Current Locale: </b>{{ getLocale() }}</div>
+            <div><b>Chosen Locale: </b>{{ $store.state.localePlugin.locale }}</div>
+            <div><b>Fallback Locale: </b>{{ getLocaleFallback() }}</div>
+            <div><b>Twitch Value: </b>{{ locale("example.key.button_example") }}</div>
+        </div>
+        <div class="button-container">
+            <button-color
+                v-bind:text="'Change region to \u0022pt_pt\u0022'"
+                v-on:click="setLocale('pt_pt')"
+            />
+            <button-color
+                v-bind:text="'Change region to \u0022en_us\u0022'"
+                v-on:click="setLocale('en_us')"
+            />
+            <button-color
+                v-bind:text="'Change region to \u0022null\u0022'"
+                v-on:click="setLocale(null)"
+            />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.locales-story > .info {
+    font-size: 18px;
+    margin: 10px 10px 10px 10px;
+}
+
+.locales-story > .button-container {
+    margin: 10px 10px 10px 10px;
+}
+
+.locales-story > .button-container > .button-color {
+    width: 250px;
+}
+</style>
+
+<script>
+import { localeMixin } from "../locales";
+
+export const LocalesStory = {
+    name: "locales-story",
+    mixins: [localeMixin]
+};
+
+export default LocalesStory;
+</script>
--- vue/mixins/stories/locales.stories.js
+++ vue/mixins/stories/locales.stories.js
@@ -1,44 +1,38 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
 import Vuex from "vuex";
 
 import { localeMixin } from "../locales";
 
-storiesOf("Mixins/Locale Mixin", module)
-    .addDecorator(withKnobs)
-    .add("Locale Mixin", () => ({
-        store: new Vuex.Store(),
-        mixins: [localeMixin],
-        props: {
-            localLocales: {
-                default: () => ({
-                    en_us: { "example.key.button_example": "Button Example" },
-                    pt_pt: { "example.key.button_example": "Botão Example" }
-                })
-            },
-            localeValue: {
-                type: String,
-                default: "en_us"
-            },
-            localeFallbackValue: {
-                type: String,
-                default: "en_us"
-            }
+import { LocalesStory } from "./locales-story";
+
+storiesOf("Mixins/Locale Mixin", module).add("Locale Mixin", () => ({
+    components: { "locales-story": LocalesStory },
+    store: new Vuex.Store(),
+    props: {
+        localLocales: {
+            default: () => ({
+                en_us: { "example.key.button_example": "Button Example" },
+                pt_pt: { "example.key.button_example": "Botão Example" }
+            })
         },
-        created: function() {
-            localeMixin.methods.setupLocalePlugin(
-                this.$store,
-                this.localLocales,
-                this.locale,
-                this.localeFallback
-            );
+        localeValue: {
+            type: String,
+            default: "en_us"
         },
-        template: `
-            <div>
-                <div><b>Current Locale: </b>{{ getLocale() }}</div>
-                <div><b>Chosen Locale: </b>{{ $store.state.localePlugin.locale }}</div>
-                <div><b>Fallback Locale: </b>{{ getLocaleFallback() }}</div>
-                <div><b>Test Value: </b>{{ locale("example.key.button_example") }}</div>
-            </div>
+        localeFallbackValue: {
+            type: String,
+            default: "en_us"
+        }
+    },
+    created: function() {
+        localeMixin.methods.setupLocalePlugin(
+            this.$store,
+            this.localLocales,
+            this.locale,
+            this.localeFallback
+        );
+    },
+    template: `
+            <locales-story />
         `
-    }));
+}));
