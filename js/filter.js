924ef5b9139c56b50d993766211e6592d2ee93f5
BeeMargarida <ms@platforme.com>
2021-02-22 17:49:18 +0000

    feat: keywords with operators

--- js/filter.js
+++ js/filter.js
@@ -8,56 +8,70 @@ const OP_ALIAS = {
 };
 
 const KEYWORDS = {
-    "@today": field => {
+    "@today": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const tomorrow = new Date(today);
         tomorrow.setDate(today.getDate() + 1);
 
+        if (op === ">" || op === ">=") return `${field}${op}${tomorrow.getTime()}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${today.getTime()}`;
         return `${field}>=${today.getTime()} and ${field}<${tomorrow.getTime()}`;
     },
-    "@yesterday": field => {
+    "@yesterday": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const todayTime = today.getTime();
         const yesterday = new Date(today);
         yesterday.setDate(today.getDate() - 1);
 
+        if (op === ">" || op === ">=") return `${field}${op}${todayTime}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${yesterday.getTime()}`;
         return `${field}>=${yesterday.getTime()} and ${field}<${todayTime}`;
     },
-    "@tomorrow": field => {
+    "@tomorrow": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const tomorrow = new Date(today.setDate(today.getDate() + 1));
         const afterTomorrow = new Date(today.setDate(today.getDate() + 1));
 
+        if (op === ">" || op === ">=") return `${field}${op}${afterTomorrow.getTime()}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${tomorrow.getTime()}`;
         return `${field}>=${tomorrow.getTime()} and ${field}<${afterTomorrow.getTime()}`;
     },
-    "@this-week": field => {
+    "@this-week": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const week = new Date(today.setDate(today.getDate() - today.getDay()));
         const nextWeek = new Date(today.setDate(today.getDate() - today.getDay() + 7));
 
+        if (op === ">" || op === ">=") return `${field}${op}${nextWeek.getTime()}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${week.getTime()}`;
         return `${field}>=${week.getTime()} and ${field}<${nextWeek.getTime()}`;
     },
-    "@next-week": field => {
+    "@next-week": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const nextWeek = new Date(today.setDate(today.getDate() - today.getDay() + 7));
         const nextNextWeek = new Date(today.setDate(today.getDate() - today.getDay() + 7));
 
+        if (op === ">" || op === ">=") return `${field}${op}${nextNextWeek.getTime()}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${nextWeek.getTime()}`;
         return `${field}>=${nextWeek.getTime()} and ${field}<${nextNextWeek.getTime()}`;
     },
-    "@this-month": field => {
+    "@this-month": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const month = today.getMonth();
         const startOfMonth = new Date(today.setDate(1));
         const nextMonth = new Date(new Date(today.setMonth(month + 1)).setDate(1));
 
+        if (op === ">" || op === ">=") return `${field}${op}${nextMonth.getTime()}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${startOfMonth.getTime()}`;
         return `${field}>=${startOfMonth.getTime()} and ${field}<${nextMonth.getTime()}`;
     },
-    "@next-month": field => {
+    "@next-month": (field, op = "=") => {
         const today = new Date(new Date().setHours(0, 0, 0, 0));
         const month = today.getMonth();
         const nextMonth = new Date(new Date(today.setMonth(month + 1)).setDate(1));
         const nextNextMonth = new Date(new Date(today.setMonth(month + 2)).setDate(1));
 
+        if (op === ">" || op === ">=") return `${field}${op}${nextNextMonth.getTime()}`;
+        else if (op === "<" || op === "<=") return `${field}${op}${nextMonth.getTime()}`;
         return `${field}>=${nextMonth.getTime()} and ${field}<${nextNextMonth.getTime()}`;
     }
 };
@@ -121,23 +135,14 @@ const _filterKeywords = (filter, nameAlias = {}, keywordFields = []) => {
     // a keyword with no key will be replaced with the fields provided in
     // the `keywordFields` argument
     for (const keyword of keywords) {
-        const result = keyword.match(OP_REGEX);
-        if (!result) {
-            // replaces the keyword with no key by the
-            // query with the keys passed as argument
-            let replaceS = "";
-            for (const i in keywordFields) {
-                replaceS += KEYWORDS[keyword](keywordFields[i]);
-                if (i < keywordFields.length - 1) replaceS += " and ";
-            }
-            filter = filter.replace(keyword, replaceS);
-        } else {
-            // replaces the key and keyword with the translation
-            // of the keyword
-            const [field, value] = keyword.split(OP_REGEX, 2);
-            if (!(value in KEYWORDS)) continue;
-            filter = filter.replace(keyword, KEYWORDS[value](field));
-        }
+        const arithOp = keyword.match(OP_REGEX);
+        if (!arithOp) return;
+
+        // replaces the key and keyword with the translation
+        // of the keyword
+        const [field, value] = keyword.split(OP_REGEX, 2);
+        if (!(value in KEYWORDS)) continue;
+        filter = filter.replace(keyword, KEYWORDS[value](field, arithOp));
     }
 
     return filter;
