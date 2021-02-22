339e010624b60976898a3c6b2e01cdf3a4885d3e
NFSS10 <nuno_nfss96@hotmail.com>
2021-02-22 10:21:16 +0000

    feat: add "should check if the value is localized" test

--- vue/test/mixins/locales/locales.test.js
+++ vue/test/mixins/locales/locales.test.js
@@ -78,4 +78,36 @@ describe("Locales Mixin", () => {
         this.testComponent.destroyLocalePlugin();
         assert.deepStrictEqual(this.testComponent.$store.state, {});
     });
+
+    it("should check if the value is localized", () => {
+        this.testComponent.setupLocalePlugin(
+            this.testComponent.$store,
+            {
+                en_us: { "example.key.button_example": "Button Example" },
+                pt_pt: { "example.key.button_example": "Botão Example" }
+            },
+            "en_us",
+            "en_us"
+        );
+        assert.deepStrictEqual(this.testComponent.$store.state, {
+            localePlugin: {
+                locale: "en_us",
+                localeFallback: "en_us",
+                locales: {
+                    en_us: {
+                        "example.key.button_example": "Button Example"
+                    },
+                    pt_pt: {
+                        "example.key.button_example": "Botão Example"
+                    }
+                }
+            }
+        });
+
+        assert.strictEqual(this.testComponent.isValueLocalized("example.not.added"), false);
+        assert.strictEqual(this.testComponent.isValueLocalized("example.key.button_example"), true);
+        assert.strictEqual(this.testComponent.isValueLocalized("example.key.button_example", "en_us"), true);
+        assert.strictEqual(this.testComponent.isValueLocalized("example.key.button_example", "pt_pt"), true);
+        assert.strictEqual(this.testComponent.isValueLocalized("example.key.button_example", "en_gb"), false);
+    });
 });
