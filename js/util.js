8fcd524f43ad7f249a14099d3ff4b6680000d99e
João Magalhães <joamag@gmail.com>
2021-03-01 15:56:44 +0000

    docs: new comments added

--- js/util.js
+++ js/util.js
@@ -1,17 +1,26 @@
+/**
+ * Normalizes the provided string value replacing irregular
+ * values with simple ones.
+ *
+ * @param {String} value The value to be normalized.
+ * @returns {String} The final normalized values.
+ */
 export const normalize = value => {
     return value.split("_").join(" ");
 };
 
+/**
+ * Creates a slug like representation of the provided value
+ * converting for instance "hello World" to "hello-world".
+ *
+ * @param {String} value The value that is going to be "slugified".
+ * @returns {String} The "slugified" version of the provided value.
+ */
 export const buildSlug = value => {
-    return (
-        value
-            .toLowerCase()
-            .trim()
-            // replace spaces with -
-            .replace(/\s+/g, "-")
-            // remove all non-word chars
-            .replace(/[^\w\-]+/g, "")
-            // replace multiple - with a single -
-            .replace(/\-\-+/g, "-")
-    );
+    return value
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-");
 };
