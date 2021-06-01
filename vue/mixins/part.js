55841a30e75053e93bba8c5eb300f9e1f2dd5e11
João Magalhães <joamag@gmail.com>
2021-06-01 15:42:41 +0100

    fix: renamed variable

--- vue/mixins/part.js
+++ vue/mixins/part.js
@@ -5,16 +5,16 @@ export const partMixin = {
          * properly notified about the problem.
          * Typical handling involves router redirection.
          *
-         * @param {Error} err The error object to be used in the
+         * @param {Error} error The error object to be used in the
          * handling of the error.
          * @param {String} message The message to be used in for the
          * error handling if not provided the default one extracted
          * from the error is used.
          */
-        handleError(err, message, code) {
+        handleError(error, message, code) {
             if (!this.$root.$router) return;
-            code = code || err.code;
-            const query = { message: message || err.message };
+            code = code || error.code;
+            const query = { message: message || error.message };
             if (code) query.code = code;
             this.$root.$router.push({
                 name: "error",
