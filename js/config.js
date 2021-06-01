16040c4afe69ec33c793aa961245626c6525ab93
Gabriel Candal <gcc@platforme.com>
2021-06-01 12:21:56 +0100

    feat: move some functions to ripe-commons

--- js/config.js
+++ js/config.js
@@ -1,30 +1,7 @@
-const features = { "*": true };
-const listeners = [];
+import { hasFeature as _hasFeature, setFeature as _setFeature, setFeatures as _setFeatures, bindFeature as _bindFeature, unbindFeature as _unbindFeature } from "ripe-commons";
 
-export const hasFeature = (name, fallback = false, source = null) => {
-    source = source || features;
-    fallback = source["*"] === undefined ? fallback : source["*"];
-    return Boolean(source[name] === undefined ? fallback : source[name]);
-};
-
-export const setFeature = (name, value) => {
-    features[name] = value;
-    listeners.forEach(callable => callable(name, value));
-};
-
-export const setFeatures = _features => {
-    Object.keys(features).forEach(k => delete features[k]);
-    Object.entries(_features).forEach(([k, v]) => {
-        features[k] = v;
-    });
-};
-
-export const bindFeature = callable => {
-    listeners.push(callable);
-    return callable;
-};
-
-export const unbindFeature = callable => {
-    listeners.splice(listeners.indexOf(callable), 1);
-    return callable;
-};
+export const hasFeature = _hasFeature;
+export const setFeature = _setFeature;
+export const setFeatures = _setFeatures;
+export const bindFeature = _bindFeature;
+export const unbindFeature = _unbindFeature;
