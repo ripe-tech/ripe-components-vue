cf45002a3568d63e8ec7c7b03adc75f38c564d46
João Magalhães <joamag@gmail.com>
2021-06-01 14:35:33 +0100

    feat: changed export structure

--- js/config.js
+++ js/config.js
@@ -1,13 +1,1 @@
-import {
-    hasFeature as _hasFeature,
-    setFeature as _setFeature,
-    setFeatures as _setFeatures,
-    bindFeature as _bindFeature,
-    unbindFeature as _unbindFeature
-} from "ripe-commons";
-
-export const hasFeature = _hasFeature;
-export const setFeature = _setFeature;
-export const setFeatures = _setFeatures;
-export const bindFeature = _bindFeature;
-export const unbindFeature = _unbindFeature;
+export { hasFeature, setFeature, setFeatures, bindFeature, unbindFeature } from "ripe-commons";
