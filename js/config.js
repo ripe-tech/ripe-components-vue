034046b73b61c464b4ad437e56966cca3a589d41
Gabriel Candal <gcc@platforme.com>
2021-06-01 12:23:49 +0100

    style: better import

--- js/config.js
+++ js/config.js
@@ -1,4 +1,10 @@
-import { hasFeature as _hasFeature, setFeature as _setFeature, setFeatures as _setFeatures, bindFeature as _bindFeature, unbindFeature as _unbindFeature } from "ripe-commons";
+import {
+    hasFeature as _hasFeature,
+    setFeature as _setFeature,
+    setFeatures as _setFeatures,
+    bindFeature as _bindFeature,
+    unbindFeature as _unbindFeature
+} from "ripe-commons";
 
 export const hasFeature = _hasFeature;
 export const setFeature = _setFeature;
