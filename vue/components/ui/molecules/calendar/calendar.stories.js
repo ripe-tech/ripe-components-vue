9a46216f7b258f5644205c10b1dbd715cb9617e8
3rdvision <kazgru@gmail.com>
2021-06-15 11:36:20 +0100

    refactor: adapt storybook to removed prop header

--- vue/components/ui/molecules/calendar/calendar.stories.js
+++ vue/components/ui/molecules/calendar/calendar.stories.js
@@ -1,15 +1,10 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 
 storiesOf("Components/Molecules/Calendar", module)
     .addDecorator(withKnobs)
     .add("Calendar", () => ({
-        props: {
-            header: {
-                default: boolean("Header", false)
-            }
-        },
         template: `
-            <calendar v-bind:header="header" />
+            <calendar />
         `
     }));
