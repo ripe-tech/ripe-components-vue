7f8e5299b81e7dedb6486020b22525269e20cee7
Mauro Benedito <v-mb@platforme.com>
2021-07-08 08:29:47 +0100

    style: pretty

--- vue/mixins/refresh.js
+++ vue/mixins/refresh.js
@@ -1,9 +1,10 @@
 export const refreshMixin = {
     mounted: function() {
-        this.refreshTimeInterval = setInterval(
-            () => this.refresh({ auto: true }),
-            this.autoRefreshTime || 6000
-        );
+        this.refreshTimeInterval = setInterval(async () => {
+            this.autoRefreshing = true;
+            await this.refresh({ auto: true });
+            this.autoRefreshing = false;
+        }, this.autoRefreshTime || 6000);
     },
     destroyed: async function() {
         clearInterval(this.refreshTimeInterval);
