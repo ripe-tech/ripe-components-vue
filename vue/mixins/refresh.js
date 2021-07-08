d61977574d42f34ce6f2ceb079a103d7958bdd0e
Mauro Benedito <v-mb@platforme.com>
2021-07-08 08:34:41 +0100

    refactor: in refresh mixin

--- vue/mixins/refresh.js
+++ vue/mixins/refresh.js
@@ -1,12 +1,17 @@
 export const refreshMixin = {
     mounted: function() {
-        this.refreshTimeInterval = setInterval(async () => {
-            this.autoRefreshing = true;
-            await this.refresh({ auto: true });
-            this.autoRefreshing = false;
-        }, this.autoRefreshTime || 6000);
+        this._autoRefresh();
     },
     destroyed: async function() {
         clearInterval(this.refreshTimeInterval);
+    },
+    methods: {
+        _autoRefresh() {
+            this.refreshTimeInterval = setInterval(async () => {
+                this.autoRefreshing = true;
+                await this.refresh({ auto: true });
+                this.autoRefreshing = false;
+            }, this.autoRefreshTime || 6000);
+        }
     }
 };
