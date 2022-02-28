8eb95a18c7f26c696774415d330498de7f140d14
Pedro Reis <pedroreis.ad@protonmail.com>
2022-02-28 17:09:27 +0000

    feat: added storybook to component
    Signed-off-by: Pedro Reis <pedroreis.ad@protonmail.com>

--- vue/components/ui/molecules/section-expandable/section-expandable.stories.js
+++ vue/components/ui/molecules/section-expandable/section-expandable.stories.js
@@ -1,17 +1,18 @@
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, text } from "@storybook/addon-knobs";
 
-storiesOf("Components/Molecules/Section", module)
+storiesOf("Components/Molecules/Section Expandable", module)
     .addDecorator(withKnobs)
     .add("Section Expandable", () => ({
         props: {
             title: {
-                default: text("Title", "Section title")
+                default: text("Title", "Expandable Section Title")
             }
         },
         template: `
-            <section-ripe
-                v-bind:title="title"
-            />
+            <section-expandable-ripe v-bind:title="title">
+                <h3> This is some text inside the expandable section. </h3>
+                <p> And some smaller text. </p>
+            </section-expandable-ripe>
         `
     }));
