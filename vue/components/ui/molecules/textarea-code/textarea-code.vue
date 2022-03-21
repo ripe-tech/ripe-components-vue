21df595a8ac209a95cba4ce71c104a02db1aa339
Nuno Silva <nuno_nfss96@hotmail.com>
2022-03-21 14:32:03 +0000

    feat: testing overlay

--- vue/components/ui/molecules/textarea-code/textarea-code.vue
+++ vue/components/ui/molecules/textarea-code/textarea-code.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="textarea-code">
-        Hello from textarea-code!
+        <textarea-ripe v-bind:textarea-props="textareaProps" />
+        <div class="overlay">
+            <div class="lines-number">
+                <div class="num">1</div>
+                <div class="num">2</div>
+                <div class="num">3</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .textarea-code {
-    font-size: 20px;
-    color: #00ff00;
+    position: relative;
+    font-size: 13px;
+    font-weight: 500;
+}
+
+.textarea-code .overlay {
+    top: 0;
+    left: 0;
+    margin: 0px 0px 0px 0px;
+    position: absolute;
+    background-color: #ff00ff40;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
 }
 </style>
 
 <script>
 export const TextareaCode = {
     name: "textarea-code",
-    props: {}
+    props: {
+        textareaProps: {
+            type: Object,
+            default: () => ({})
+        }
+    }
 };
 
 export default TextareaCode;
