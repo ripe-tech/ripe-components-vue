76916113e28e1f06f58ef3544ad930622911abde
NFSS10 <nuno_nfss96@hotmail.com>
2022-03-24 15:02:36 +0000

    feat: syntax overlay properly aligned

--- vue/components/ui/molecules/code-editor/code-editor.vue
+++ vue/components/ui/molecules/code-editor/code-editor.vue
@@ -2,37 +2,66 @@
     <div class="code-editor">
         <textarea-ripe v-bind:value="value" v-bind:monospaced="true" v-bind:resize="true" />
         <div class="overlay">
-            <div class="lines-number">
-                <div class="line" v-for="(line, index) in lines" v-bind:key="index">
-                    <div class="number">{{ index }}</div>
-                    <pre><div class="text">{{ line }}</div></pre>
-                </div>
+            <div class="lines">
+                <div class="line" v-for="(line, index) in lines" v-bind:key="index">{{ line }}</div>
+                <!-- <div class="line" v-for="(line, index) in lines" v-bind:key="index"><pre>{{ line }}</pre></div> -->
+                <!-- <div class="line" v-for="(line, index) in lines" v-bind:key="index"> -->
+                    <!-- <div class="number">{{ index }}</div> -->
+                   <!--  <pre><div class="text">{{ line }}</div></pre> -->
+                <!-- </div> -->
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@import "css/variables.scss";
+
+// TODO scroll, adicionar overflow e sincronizar scroll da textarea com o overlay
+
 .code-editor {
     position: relative;
-    font-size: 13px;
-    font-weight: 500;
+}
+
+.code-editor > textarea,
+.code-editor > .overlay {
+    padding: 8px 12px 8px 12px;
+    border: 1px solid $light-white;
+    border-radius: 6px 6px 6px 6px;
+    box-sizing: border-box;
+
     font-family: "consolas", monospace;
     letter-spacing: 0px;
     line-height: 18px;
+    font-size: 13px;
+    font-weight: 500;
+    outline: none;
 }
 
-.code-editor .overlay {
+.code-editor > .overlay {
+    position: absolute;
     top: 0;
     left: 0;
-    margin: 0px 0px 0px 0px;
-    position: absolute;
-    background-color: #ff00ff40;
     width: 100%;
     height: 100%;
-    pointer-events: none;
+    white-space: pre;
+
+    padding-top: 9px;
+
+    // need to adjust
+    // padding: 8px 12px 8px 12px;
+
+    // debug
+    background-color: #ff00ff40;
+
+    // display: none;
+    // margin: 0px 0px 0px 0px;
+    // width: 100%;
+    // height: 100%;
+    // pointer-events: none;
 }
 
+/*
 .lines-number > .line {
     font-size: 13px;
     height: 13px;
@@ -43,6 +72,7 @@
     display: none;
     // margin: 0px -20px 0px 0px;
 }
+*/
 </style>
 
 <script>
