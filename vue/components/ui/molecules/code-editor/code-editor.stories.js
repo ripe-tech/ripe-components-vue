1c06e64a23d24645fb3422a3f73c11c58eb3e24e
NFSS10 <nuno_nfss96@hotmail.com>
2022-03-25 18:07:34 +0000

    feat: update `code-editor` storybook

--- vue/components/ui/molecules/code-editor/code-editor.stories.js
+++ vue/components/ui/molecules/code-editor/code-editor.stories.js
@@ -1,11 +1,31 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, boolean, select } from "@storybook/addon-knobs";
 
 storiesOf("Components/Molecules/Code Editor", module)
     .addDecorator(withKnobs)
     .add("Code Editor", () => ({
-        props: {},
+        props: {
+            value: {
+                default: JSON.stringify({ example: "exemple text", "example number": 123 }, null, 4)
+            },
+            disabled: {
+                default: boolean("Disabled", false)
+            },
+            type: {
+                default: select(
+                    "Type",
+                    {
+                        JSON: "json"
+                    },
+                    "json"
+                )
+            }
+        },
         template: `
-            <code-editor />
+            <code-editor
+                v-bind:value="value"
+                v-bind:disabled="disabled"
+                v-bind:type="type"
+            />
         `
     }));
