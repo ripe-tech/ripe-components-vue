d75aecc72770bbeccec2fee6af8db3d188316404
Pedro Reis <pedroreis.ad@protonmail.com>
2022-05-06 15:47:25 +0100

    fix: lint fix

--- vue/components/ui/molecules/avatar-list/avatar-list.vue
+++ vue/components/ui/molecules/avatar-list/avatar-list.vue
@@ -166,7 +166,7 @@ export const AvatarList = {
     },
     methods: {
         style(index) {
-            return {'z-index': index};
+            return { "z-index": index };
         }
     }
 };
