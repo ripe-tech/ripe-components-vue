67cf03586dc5e729020c8a2aeec6036ad5a85f2f
JoaoLeal12 <joaofsl1994@gmail.com>
2022-06-03 10:39:30 +0100

    feat: add disabled to button-dropdown storybook

--- vue/components/ui/molecules/button-dropdown/button-dropdown.stories.js
+++ vue/components/ui/molecules/button-dropdown/button-dropdown.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, select, text } from "@storybook/addon-knobs";
+import { withKnobs, select, text, boolean } from "@storybook/addon-knobs";
 
 storiesOf("Components/Molecules/Button Dropdown", module)
     .addDecorator(withKnobs)
@@ -41,6 +41,10 @@ storiesOf("Components/Molecules/Button Dropdown", module)
             label: {
                 default: text("Label", "Edit dropdown")
             },
+            disabled: {
+                type: Boolean,
+                default: boolean("Disabled", false)
+            },
             mockItems: {
                 type: Array,
                 default: () => [
@@ -53,6 +57,7 @@ storiesOf("Components/Molecules/Button Dropdown", module)
             <button-dropdown
                 v-bind:size="size"
                 v-bind:label="label"
+                v-bind:disabled="disabled"
                 v-bind:primary-icon="primaryIcon"
                 v-bind:secondary-icon="secondaryIcon"
                 v-bind:items="mockItems"
